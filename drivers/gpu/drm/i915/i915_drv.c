b83736c7b6abeff006eef16bbb7f385665434f00
Huang, Sean Z <sean.z.huang@intel.com>
2020-12-09 22:45:02 -0800

    CHROMIUM: drm/i915/pxp: Terminate the session upon app crash
    PXP should terminate the hardware session and cleanup the software
    state gracefully when the application has established the
    protection session, but doesn't close the session correctly due to
    some cases like application crash.
    
    Signed-off-by: Huang, Sean Z <sean.z.huang@intel.com>
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Reviewed by: Alan Previn <alan.previn.teres.alexis@intel.com>
    
    BUG=b:191508777
    TEST=tast run <IP> video.PlayDRM*
    
    Signed-off-by: Juston Li <juston.li@intel.com>
    Change-Id: Id83d6fc3969d1b26065d730550766edad6bedd72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3203918
    Tested-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -70,6 +70,7 @@
 
 #include "pxp/intel_pxp.h"
 #include "pxp/intel_pxp_pm.h"
+#include "pxp/intel_pxp.h"
 
 #include "i915_debugfs.h"
 #include "i915_drm_client.h"
@@ -1011,6 +1012,8 @@ static void i915_driver_postclose(struct drm_device *dev, struct drm_file *file)
 
 	/* Catch up with all the deferred frees from "this" client */
 	i915_gem_flush_free_objects(to_i915(dev));
+
+	intel_pxp_close(&(to_i915(dev)->gt.pxp), file);
 }
 
 static void intel_suspend_encoders(struct drm_i915_private *dev_priv)
