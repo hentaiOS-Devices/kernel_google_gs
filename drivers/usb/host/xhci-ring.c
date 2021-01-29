98d2268674c599f3cc3afabc3610130c8ca1e1b9
Mathias Nyman <mathias.nyman@linux.intel.com>
2021-01-29 15:00:43 +0200

    UPSTREAM: xhci: Check for pending reset endpoint command before queueing a new one.
    A halted endpoint can be detected both when transfer events complete, and
    in stop endpoint command completion. Both these handlers will start
    clearing up the halted endpoint and queue a reset endpoint command.
    
    It's possible to get both events for the same halted endpoint if right
    after a URB cancel queues a stop endpoint command the endpoint stalls.
    Use the EP_HALTED flag to prevent resetting the endpoint twice.
    
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Link: https://lore.kernel.org/r/20210129130044.206855-27-mathias.nyman@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 51ee4a84300200c51303ef15b84981b2edb6ec47)
    
    BUG=b:192925463
    TEST=Test S0ix for 500 cycles on brya.
    
    Signed-off-by: George D Sworo <george.d.sworo@intel.com>
    Change-Id: I10b71b228d80699486717b21307a4741db23927c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3331832
    Reviewed-by: Rajat Jain <rajatja@google.com>
    Commit-Queue: Rajat Jain <rajatja@google.com>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -938,8 +938,6 @@ static void xhci_handle_halted_endpoint(struct xhci_hcd *xhci,
 	if (ep->vdev->flags & VDEV_PORT_ERROR)
 		return;
 
-	ep->ep_state |= EP_HALTED;
-
 	/* add td to cancelled list and let reset ep handler take care of it */
 	if (reset_type == EP_HARD_RESET) {
 		ep->ep_state |= EP_HARD_CLEAR_TOGGLE;
@@ -949,9 +947,16 @@ static void xhci_handle_halted_endpoint(struct xhci_hcd *xhci,
 		}
 	}
 
+	if (ep->ep_state & EP_HALTED) {
+		xhci_dbg(xhci, "Reset ep command already pending\n");
+		return;
+	}
+
 	err = xhci_reset_halted_ep(xhci, slot_id, ep->ep_index, reset_type);
 	if (err)
 		return;
+
+	ep->ep_state |= EP_HALTED;
 
 	xhci_ring_cmd_db(xhci);
 }
@@ -2586,7 +2591,6 @@ static int handle_tx_event(struct xhci_hcd *xhci,
 	case COMP_STALL_ERROR:
 		xhci_dbg(xhci, "Stalled endpoint for slot %u ep %u\n", slot_id,
 			 ep_index);
-		ep->ep_state |= EP_HALTED;
 		status = -EPIPE;
 		break;
 	case COMP_SPLIT_TRANSACTION_ERROR:
