ea00ea802838e904a7bace6e92a97d95d526d467
Chun-Kuang Hu <chunkuang.hu@kernel.org>
2021-03-15 07:33:21 +0800

    UPSTREAM: mailbox: mtk-cmdq: Remove cmdq_cb_status
    cmdq_cb_status is an error status. Use the standard error number
    instead of cmdq_cb_status to prevent status duplication.
    
    Signed-off-by: Chun-Kuang Hu <chunkuang.hu@kernel.org>
    Reviewed-by: Yongqiang Niu <yongqiang.niu@mediatek.com>
    Signed-off-by: Jassi Brar <jaswinder.singh@linaro.org>
    (cherry picked from commit b3c0d72b092e52ae7369b52fb97f63eb2ea7f16a)
    
    BUG=b:188614535
    TEST=emerge-cherry sys-kernel/chromeos-kernel-5_10
    
    Signed-off-by: jason-jh.lin <jason-jh.lin@mediatek.com>
    Change-Id: I5e06106b0ff8431ee5000ebd5f93ead35e00d38e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3244691
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Tested-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Commit-Queue: Tzung-Bi Shih <tzungbi@chromium.org>

--- drivers/mailbox/mtk-cmdq-mailbox.c
+++ drivers/mailbox/mtk-cmdq-mailbox.c
@@ -200,7 +200,7 @@ static bool cmdq_thread_is_in_wfe(struct cmdq_thread *thread)
 	return readl(thread->base + CMDQ_THR_WAIT_TOKEN) & CMDQ_THR_IS_WAITING;
 }
 
-static void cmdq_task_exec_done(struct cmdq_task *task, enum cmdq_cb_status sta)
+static void cmdq_task_exec_done(struct cmdq_task *task, int sta)
 {
 	struct cmdq_task_cb *cb = &task->pkt->async_cb;
 	struct cmdq_cb_data data;
@@ -264,10 +264,10 @@ static void cmdq_thread_irq_handler(struct cmdq *cmdq,
 			curr_task = task;
 
 		if (!curr_task || curr_pa == task_end_pa - CMDQ_INST_SIZE) {
-			cmdq_task_exec_done(task, CMDQ_CB_NORMAL);
+			cmdq_task_exec_done(task, 0);
 			kfree(task);
 		} else if (err) {
-			cmdq_task_exec_done(task, CMDQ_CB_ERROR);
+			cmdq_task_exec_done(task, -ENOEXEC);
 			cmdq_task_handle_error(curr_task);
 			kfree(task);
 		}
@@ -436,7 +436,7 @@ static void cmdq_mbox_shutdown(struct mbox_chan *chan)
 
 	list_for_each_entry_safe(task, tmp, &thread->task_busy_list,
 				 list_entry) {
-		cmdq_task_exec_done(task, CMDQ_CB_ERROR);
+		cmdq_task_exec_done(task, -ECONNABORTED);
 		kfree(task);
 	}
 
@@ -475,7 +475,7 @@ static int cmdq_mbox_flush(struct mbox_chan *chan, unsigned long timeout)
 				 list_entry) {
 		cb = &task->pkt->async_cb;
 		if (cb->cb) {
-			data.sta = CMDQ_CB_ERROR;
+			data.sta = -ECONNABORTED;
 			data.data = cb->data;
 			cb->cb(data);
 		}
