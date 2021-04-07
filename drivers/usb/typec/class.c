98e43a40b551c910efd7c6afaa32b66c67d22dc4
Heikki Krogerus <heikki.krogerus@linux.intel.com>
2021-04-07 09:55:52 +0300

    UPSTREAM: usb: typec: Port mapping utility
    Adding functions that can be used to link/unlink ports -
    USB ports, TBT3/USB4 ports, DisplayPorts and so on - to
    the USB Type-C connectors they are attached to inside a
    system. The symlink that is created for the port device is
    named "connector".
    
    Initially only ACPI is supported. ACPI port object shares
    the _PLD (Physical Location of Device) with the USB Type-C
    connector that it's attached to.
    
    Signed-off-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Link: https://lore.kernel.org/r/20210407065555.88110-2-heikki.krogerus@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit ae196ddb0d3186bc08e529b8ea4bf62161ddfce2)
    
    BUG=b:202446737
    TEST=Verify that connected USB devices point to the right Type C port
         device.
    
    Signed-off-by: Prashant Malani <pmalani@chromium.org>
    Change-Id: I18c50793841530f6b1e80468711ee7f008b2bc13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3213459
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Benson Leung <bleung@google.com>

--- drivers/usb/typec/class.c
+++ drivers/usb/typec/class.c
@@ -18,7 +18,7 @@
 
 static DEFINE_IDA(typec_index_ida);
 
-static struct class typec_class = {
+struct class typec_class = {
 	.name = "typec",
 	.owner = THIS_MODULE,
 };
@@ -1603,6 +1603,7 @@ static void typec_release(struct device *dev)
 	ida_destroy(&port->mode_ids);
 	typec_switch_put(port->sw);
 	typec_mux_put(port->mux);
+	free_pld(port->pld);
 	kfree(port->cap);
 	kfree(port);
 }
@@ -1985,6 +1986,8 @@ struct typec_port *typec_register_port(struct device *parent,
 
 	ida_init(&port->mode_ids);
 	mutex_init(&port->port_type_lock);
+	mutex_init(&port->port_list_lock);
+	INIT_LIST_HEAD(&port->port_list);
 
 	port->id = id;
 	port->ops = cap->ops;
@@ -2025,6 +2028,8 @@ struct typec_port *typec_register_port(struct device *parent,
 		put_device(&port->dev);
 		return ERR_PTR(ret);
 	}
+
+	port->pld = get_pld(&port->dev);
 
 	return port;
 }
