303e786947e52a4bf6ee55cc82a832a32ce4d980
Miaohe Lin <linmiaohe@huawei.com>
2021-05-04 18:34:17 -0700

    khugepaged: remove unnecessary out label in collapse_huge_page()
    The out label here is unneeded because it just goes to out_up_write label.
    Remove it to make code more concise.
    
    Link: https://lkml.kernel.org/r/20210325135647.64106-3-linmiaohe@huawei.com
    Signed-off-by: Miaohe Lin <linmiaohe@huawei.com>
    Reviewed-by: Zi Yan <ziy@nvidia.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1130,10 +1130,10 @@ static void collapse_huge_page(struct mm_struct *mm,
 	mmap_write_lock(mm);
 	result = hugepage_vma_revalidate(mm, address, &vma);
 	if (result)
-		goto out;
+		goto out_up_write;
 	/* check if the pmd is still valid */
 	if (mm_find_pmd(mm, address) != pmd)
-		goto out;
+		goto out_up_write;
 
 	vm_write_begin(vma);
 	anon_vma_lock_write(vma->anon_vma);
@@ -1178,7 +1178,7 @@ static void collapse_huge_page(struct mm_struct *mm,
 		anon_vma_unlock_write(vma->anon_vma);
 		vm_write_end(vma);
 		result = SCAN_FAIL;
-		goto out;
+		goto out_up_write;
 	}
 
 	/*
@@ -1223,8 +1223,6 @@ out_nolock:
 		mem_cgroup_uncharge(*hpage);
 	trace_mm_collapse_huge_page(mm, isolated, result);
 	return;
-out:
-	goto out_up_write;
 }
 
 static int khugepaged_scan_pmd(struct mm_struct *mm,
