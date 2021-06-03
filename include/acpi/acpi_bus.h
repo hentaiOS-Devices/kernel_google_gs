39260c9cb3f075d3c657d75967971a43cbb96b7d
Daniel Scally <djrscally@gmail.com>
2021-06-03 23:40:02 +0100

    UPSTREAM: ACPI: scan: Extend acpi_walk_dep_device_list()
    The acpi_walk_dep_device_list() function is not as generic as its
    name implies, serving only to decrement the dependency count for each
    dependent device of the input.
    
    Extend it to accept a callback which can be applied to all the
    dependencies in acpi_dep_list.
    
    Replace all existing calls to the function with calls to a wrapper,
    passing a callback that applies the same dependency reduction.
    
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Acked-by: Maximilian Luz <luzmaximilian@gmail.com>  # for platform/surface parts
    Signed-off-by: Daniel Scally <djrscally@gmail.com>
    [ rjw: Changelog edits ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit a9e10e58730432e5de840eb3ddd55c75f29341b3)
    
    BUG=b:233114312
    TEST=Basic boot on skolas
    
    Signed-off-by: George D Sworo <george.d.sworo@intel.com>
    Change-Id: I3d677012f3a221d55d2fe57338627eb0977579f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3711249
    Owners-Override: Lalith Rajendran <lalithkraj@google.com>
    Reviewed-by: Lalith Rajendran <lalithkraj@google.com>
    Commit-Queue: Lalith Rajendran <lalithkraj@google.com>
    Tested-by: Lalith Rajendran <lalithkraj@google.com>

--- include/acpi/acpi_bus.h
+++ include/acpi/acpi_bus.h
@@ -280,6 +280,12 @@ struct acpi_device_power {
 	u8 state_for_enumeration; /* Maximum power state for enumeration */
 };
 
+struct acpi_dep_data {
+	struct list_head node;
+	acpi_handle supplier;
+	acpi_handle consumer;
+};
+
 /* Performance Management */
 
 struct acpi_device_perf_flags {
@@ -690,6 +696,7 @@ static inline bool acpi_device_can_poweroff(struct acpi_device *adev)
 
 bool acpi_dev_hid_uid_match(struct acpi_device *adev, const char *hid2, const char *uid2);
 
+void acpi_dev_clear_dependencies(struct acpi_device *supplier);
 struct acpi_device *
 acpi_dev_get_first_match_dev(const char *hid, const char *uid, s64 hrv);
 
