96e17b504decc5a189318c8808caee187993ab41
Chris Wilson <chris@chris-wilson.co.uk>
2021-06-17 08:30:13 +0200

    BACKPORT: drm/i915/gt: Pipelined page migration
    If we pipeline the PTE updates and then do the copy of those pages
    within a single unpreemptible command packet, we can submit the copies
    and leave them to be scheduled without having to synchronously wait
    under a global lock. In order to manage migration, we need to
    preallocate the page tables (and keep them pinned and available for use
    at any time), causing a bottleneck for migrations as all clients must
    contend on the limited resources. By inlining the ppGTT updates and
    performing the blit atomically, each client only owns the PTE while in
    use, and so we can reschedule individual operations however we see fit.
    And most importantly, we do not need to take a global lock on the shared
    vm, and wait until the operation is complete before releasing the lock
    for others to claim the PTE for themselves.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Co-developed-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Signed-off-by: Matthew Auld <matthew.auld@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210617063018.92802-8-thomas.hellstrom@linux.intel.com
    (cherry picked from commit cf586021642d8017cde111b7dd1ba86224e9da51)
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    
    Downstream changes:
    -Conflict with downstream PXP support 0b355579e4ea ("FROMGIT: BACKPORT:
     drm/i915/pxp: allocate a vcs context for pxp usage")
    
    BUG=b:207759642
    TEST=Tested on brya/zork/tomato/trogdor/grunt
    
    Backported using forklift.py
    
    Change-Id: I44cd20b66d330928ed09114606bb9fb2181b6e85

--- drivers/gpu/drm/i915/gt/intel_engine.h
+++ drivers/gpu/drm/i915/gt/intel_engine.h
@@ -188,6 +188,7 @@ intel_write_status_page(struct intel_engine_cs *engine, int reg, u32 value)
 #define I915_GEM_HWS_PREEMPT_ADDR	(I915_GEM_HWS_PREEMPT * sizeof(u32))
 #define I915_GEM_HWS_SEQNO		0x40
 #define I915_GEM_HWS_SEQNO_ADDR		(I915_GEM_HWS_SEQNO * sizeof(u32))
+#define I915_GEM_HWS_MIGRATE		(0x42 * sizeof(u32))
 #define I915_GEM_HWS_PXP		0x60
 #define I915_GEM_HWS_PXP_ADDR		(I915_GEM_HWS_PXP * sizeof(u32))
 #define I915_GEM_HWS_SCRATCH		0x80
