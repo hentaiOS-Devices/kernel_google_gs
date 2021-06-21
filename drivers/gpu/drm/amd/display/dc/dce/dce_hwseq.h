112704461afcc8f97f18ba685cf0f88c3d3f1320
Alex Deucher <alexander.deucher@amd.com>
2021-06-21 16:51:30 -0400

    BACKPORT: drm/amdgpu/display: fold DRM_AMD_DC_DCN3_1 into DRM_AMD_DC_DCN
    No need for a separate flag now that DCN3.1 is not in bring up.
    Fold into DRM_AMD_DC_DCN like previous DCN IPs.
    
    Reviewed-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 8fe44c080a53ac0ccbe88053a2e40f9acca33091)
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    
    Downstream changes:
    -Resolved context conflict with downstream backport 4ed1a5f50b6e
     ("BACKPORT: drm/amd/display: Respect CONFIG_FRAME_WARN=0 in dml
     Makefile")
    
    BUG=b:191366271
    TEST=Tested on volteer/zork/trogdor/grunt/kukui
    
    Backported using forklift.py
    
    Change-Id: I841c6991eec7007841ee8c14529dd5a741174307

--- drivers/gpu/drm/amd/display/dc/dce/dce_hwseq.h
+++ drivers/gpu/drm/amd/display/dc/dce/dce_hwseq.h
@@ -1107,7 +1107,6 @@ struct dce_hwseq_registers {
 	type PANEL_DIGON_OVRD;\
 	type PANEL_PWRSEQ_TARGET_STATE_R;
 
-#if defined(CONFIG_DRM_AMD_DC_DCN3_1)
 #define HWSEQ_DCN31_REG_FIELD_LIST(type) \
 	type DOMAIN_POWER_FORCEON;\
 	type DOMAIN_POWER_GATE;\
@@ -1115,15 +1114,12 @@ struct dce_hwseq_registers {
 	type HPO_HDMISTREAMCLK_G_GATE_DIS;\
 	type DISABLE_HOSTVM_FORCE_ALLOW_PSTATE;
 
-#endif
 struct dce_hwseq_shift {
 	HWSEQ_REG_FIELD_LIST(uint8_t)
 	HWSEQ_DCN_REG_FIELD_LIST(uint8_t)
 	HWSEQ_DCN3_REG_FIELD_LIST(uint8_t)
 	HWSEQ_DCN301_REG_FIELD_LIST(uint8_t)
-#if defined(CONFIG_DRM_AMD_DC_DCN3_1)
 	HWSEQ_DCN31_REG_FIELD_LIST(uint8_t)
-#endif
 };
 
 struct dce_hwseq_mask {
@@ -1131,9 +1127,7 @@ struct dce_hwseq_mask {
 	HWSEQ_DCN_REG_FIELD_LIST(uint32_t)
 	HWSEQ_DCN3_REG_FIELD_LIST(uint32_t)
 	HWSEQ_DCN301_REG_FIELD_LIST(uint32_t)
-#if defined(CONFIG_DRM_AMD_DC_DCN3_1)
 	HWSEQ_DCN31_REG_FIELD_LIST(uint32_t)
-#endif
 };
 
 
