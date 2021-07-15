2039b3bfce340cd471e4b72667e96897d8119789
Benjamin Gaignard <benjamin.gaignard@collabora.com>
2021-07-15 17:12:22 +0200

    BACKPORT: media: hevc: Add scaling matrix control
    HEVC scaling lists are used for the scaling process for transform
    coefficients.
    V4L2_HEVC_SPS_FLAG_SCALING_LIST_ENABLED has to set when they are
    encoded in the bitstream.
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@collabora.com>
    Reviewed-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Reviewed-by: Ezequiel Garcia <ezequiel@collabora.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    (cherry picked from commit 7ba59fb6c3b473dc0c76e87cd493388480c6dd27)
    
    Conflicts:
       drivers/media/v4l2-core/v4l2-ctrls-core.c
    
    BUG=b:241350291
    TEST=Build for Cherry
    
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>
    Change-Id: Ib057afcfdf922d117d848986b40aeafc65de1da8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3971775
    Reviewed-by: Steve Cho <stevecho@chromium.org>

--- drivers/media/v4l2-core/v4l2-ctrls-core.c
+++ drivers/media/v4l2-core/v4l2-ctrls-core.c
@@ -1112,6 +1112,9 @@ static int std_validate_compound(const struct v4l2_ctrl *ctrl, u32 idx,
 
 		break;
 
+	case V4L2_CTRL_TYPE_HEVC_SCALING_MATRIX:
+		break;
+
 	case V4L2_CTRL_TYPE_VP9_COMPRESSED_HDR:
 		return validate_vp9_compressed_hdr(p);
 
@@ -1765,6 +1768,9 @@ static struct v4l2_ctrl *v4l2_ctrl_new(struct v4l2_ctrl_handler *hdl,
 		break;
 	case V4L2_CTRL_TYPE_HEVC_SLICE_PARAMS:
 		elem_size = sizeof(struct v4l2_ctrl_hevc_slice_params);
+		break;
+	case V4L2_CTRL_TYPE_HEVC_SCALING_MATRIX:
+		elem_size = sizeof(struct v4l2_ctrl_hevc_scaling_matrix);
 		break;
 	case V4L2_CTRL_TYPE_HEVC_DECODE_PARAMS:
 		elem_size = sizeof(struct v4l2_ctrl_hevc_decode_params);
