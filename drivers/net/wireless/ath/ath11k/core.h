b5994662302d36fa10245c19cb5c873cd215e5f4
Sathishkumar Muruganandam <murugana@codeaurora.org>
2021-07-21 00:31:46 +0300

    UPSTREAM: ath11k: fix 4-addr tx failure for AP and STA modes
    Ath11k FW requires peer parameter WMI_PEER_USE_4ADDR to be set for
    4-addr peers allowing 4-address frame transmission to those peers.
    
    Add ath11k driver callback for sta_set_4addr() to queue new workq
    set_4addr_wk only once based on new boolean, use_4addr_set.
    
    sta_set_4addr() will be called during 4-addr STA association cases
    applicable for both AP and STA modes.
    
    In ath11k_sta_set_4addr_wk(),
    
    AP mode:
            WMI_PEER_USE_4ADDR will be set for the corresponding
            associated 4-addr STA(s)
    
    STA mode:
            WMI_PEER_USE_4ADDR will be set for the AP to which the
            4-addr STA got associated.
    
    Tested-on: IPQ8074 WLAN.HK.2.1.0.1-01238-QCAHKSWPL_SILICONZ-1
    
    Signed-off-by: Sathishkumar Muruganandam <murugana@codeaurora.org>
    Signed-off-by: Jouni Malinen <jouni@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20210720213147.90042-1-jouni@codeaurora.org
    (cherry picked from commit 34c67dc366419e06129dad0f32f521842bdff9bc)
    
    BUG=b:205920109
    TEST=wifi_matfunc/perf
    
    Signed-off-by: Abhishek Kumar <kuabhs@chromium.org>
    Change-Id: Ic3dc6707fb598f1092df19b10c25bb79671d59ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3350015
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

--- drivers/net/wireless/ath/ath11k/core.h
+++ drivers/net/wireless/ath/ath11k/core.h
@@ -367,6 +367,7 @@ struct ath11k_sta {
 	enum hal_pn_type pn_type;
 
 	struct work_struct update_wk;
+	struct work_struct set_4addr_wk;
 	struct rate_info txrate;
 	struct rate_info last_txrate;
 	u64 rx_duration;
@@ -379,6 +380,8 @@ struct ath11k_sta {
 	/* protected by conf_mutex */
 	bool aggr_mode;
 #endif
+
+	bool use_4addr_set;
 };
 
 #define ATH11K_MIN_5G_FREQ 4150
