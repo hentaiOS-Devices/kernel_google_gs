4803892102afde0fc9313c607f61f71ac259e8dc
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2021-08-03 14:00:37 +0900

    UPSTREAM: ASoC: soc-ops: cleanup cppcheck warning at snd_soc_put_volsw_sx()
    This patch cleanups below cppcheck warning.
    
    sound/soc/soc-ops.c:410:30: style: The scope of the variable 'val2' can be reduced. [variableScope]
     unsigned int val, val_mask, val2 = 0;
                                 ^
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/874kc7w2a2.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 58f42dfd7977599b060996491412fcec688d025d)
    
    BUG=b:218766331
    TEST=Test sound cards are listed and audio playback works.
    
    Signed-off-by: Madhulika Dolke <madhulika.dolke@intel.com>
    (cherry picked from commit 58f42dfd7977599b060996491412fcec688d025d)
    Change-Id: I9b1fc94bebfa6568946309e2b295e3ff2e65bde1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3519358
    Reviewed-by: Terry Cheong <htcheong@chromium.org>
    Commit-Queue: Terry Cheong <htcheong@chromium.org>
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Tested-by: Terry Cheong <htcheong@chromium.org>

--- sound/soc/soc-ops.c
+++ sound/soc/soc-ops.c
@@ -427,7 +427,7 @@ int snd_soc_put_volsw_sx(struct snd_kcontrol *kcontrol,
 	int min = mc->min;
 	unsigned int mask = (1U << (fls(min + max) - 1)) - 1;
 	int err = 0;
-	unsigned int val, val_mask, val2;
+	unsigned int val, val_mask;
 
 	if (ucontrol->value.integer.value[0] < 0)
 		return -EINVAL;
@@ -445,6 +445,8 @@ int snd_soc_put_volsw_sx(struct snd_kcontrol *kcontrol,
 		return err;
 
 	if (snd_soc_volsw_is_stereo(mc)) {
+		unsigned int val2;
+
 		val_mask = mask << rshift;
 		val2 = (ucontrol->value.integer.value[1] + min) & mask;
 		val2 = val2 << rshift;
