cbd6c7a69bec8095e1e1c75e69bf4b8aaf5ff17b
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2021-08-03 14:01:00 +0900

    UPSTREAM: ASoC: soc-ops: cleanup cppcheck warning at snd_soc_put_xr_sx()
    This patch cleanups below cppcheck warning.
    
    sound/soc/soc-ops.c:859:18: style: The scope of the variable 'regval' can be reduced. [variableScope]
     unsigned int i, regval, regmask;
                     ^
    sound/soc/soc-ops.c:859:26: style: The scope of the variable 'regmask' can be reduced. [variableScope]
     unsigned int i, regval, regmask;
                             ^
    sound/soc/soc-ops.c:860:6: style: The scope of the variable 'err' can be reduced. [variableScope]
     int err;
         ^
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/87zgtzunoz.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit b285b51018a7ca206401829fb83c8b967c22bfa5)
    
    BUG=b:218766331
    TEST=Test sound cards are listed and audio playback works.
    
    Signed-off-by: Madhulika Dolke <madhulika.dolke@intel.com>
    (cherry picked from commit b285b51018a7ca206401829fb83c8b967c22bfa5)
    Change-Id: Ic587b900a99531bc69af7b0553c898970cc0ef83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3519361
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Commit-Queue: Terry Cheong <htcheong@chromium.org>
    Tested-by: Terry Cheong <htcheong@chromium.org>
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Reviewed-by: Terry Cheong <htcheong@chromium.org>

--- sound/soc/soc-ops.c
+++ sound/soc/soc-ops.c
@@ -904,8 +904,7 @@ int snd_soc_put_xr_sx(struct snd_kcontrol *kcontrol,
 	unsigned long mask = (1UL<<mc->nbits)-1;
 	long max = mc->max;
 	long val = ucontrol->value.integer.value[0];
-	unsigned int i, regval, regmask;
-	int err;
+	unsigned int i;
 
 	if (val < mc->min || val > mc->max)
 		return -EINVAL;
@@ -913,10 +912,10 @@ int snd_soc_put_xr_sx(struct snd_kcontrol *kcontrol,
 		val = max - val;
 	val &= mask;
 	for (i = 0; i < regcount; i++) {
-		regval = (val >> (regwshift*(regcount-i-1))) & regwmask;
-		regmask = (mask >> (regwshift*(regcount-i-1))) & regwmask;
-		err = snd_soc_component_update_bits(component, regbase+i,
-				regmask, regval);
+		unsigned int regval = (val >> (regwshift*(regcount-i-1))) & regwmask;
+		unsigned int regmask = (mask >> (regwshift*(regcount-i-1))) & regwmask;
+		int err = snd_soc_component_update_bits(component, regbase+i,
+							regmask, regval);
 		if (err < 0)
 			return err;
 	}
