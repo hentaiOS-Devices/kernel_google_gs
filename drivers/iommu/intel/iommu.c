c19174dff49103b9904fc70d76f7d6694f7cd9b4
Robin Murphy <robin.murphy@arm.com>
2021-08-11 13:21:18 +0100

    UPSTREAM: iommu/vt-d: Drop IOVA cookie management
    The core code bakes its own cookies now.
    
    BUG=b:212288393
    TEST=Built and booted the kernel on a victus test device
    
    Link: https://lore.kernel.org/r/e9dbe3b6108f8538e17e0c5f59f8feeb714f51a4.1628682048.git.robin.murphy@arm.com
    (cherry picked from commit f297e27f831705de50811d933a394969602e8564)
    Signed-off-by: Syed Faaiz Hussain <syedfaaiz@google.com>
    Change-Id: Ie3721e21307660c15728adba610a27839aee8944
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3522108
    Reviewed-by: Dominik Behr <dbehr@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -1980,10 +1980,6 @@ static void domain_exit(struct dmar_domain *domain)
 	/* Remove associated devices and clear attached or cached domains */
 	domain_remove_dev_info(domain);
 
-	/* destroy iovas */
-	if (domain->domain.type == IOMMU_DOMAIN_DMA)
-		iommu_put_dma_cookie(&domain->domain);
-
 	if (domain->pgd) {
 		struct page *freelist;
 
@@ -4456,10 +4452,6 @@ static struct iommu_domain *intel_iommu_domain_alloc(unsigned type)
 			domain_exit(dmar_domain);
 			return NULL;
 		}
-
-		if (type == IOMMU_DOMAIN_DMA &&
-		    iommu_get_dma_cookie(&dmar_domain->domain))
-			return NULL;
 
 		domain = &dmar_domain->domain;
 		domain->geometry.aperture_start = 0;
