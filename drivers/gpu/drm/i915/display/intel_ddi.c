0c716e4a9007d2b0faab13664baf29387a43966f
José Roberto de Souza <jose.souza@intel.com>
2021-08-27 10:42:53 -0700

    UPSTREAM: drm/i915/display: Renaming DRRS functions to intel_drrs_*()
    We had a mix of intel_edp_drrs_*(), intel_dp_drrs_*() and
    intel_dp_set_drrs_state(), so properly renaming all functions to
    keep the same pattern.
    
    While at it, also dropping intel_dp_set_drrs_state from the
    documentation as it is a static function.
    
    v3:
    - dropping documentation style comment in static function
    (intel_drrs_set_state)
    
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210827174253.51122-3-jose.souza@intel.com
    (cherry picked from commit 3a3dd5342f32ddf6c41b17c1c9e125ffab92be70)
    
    BUG=b: 204686428
    TEST=Basic boot on brya and check cat /sys/kernel/debug/dri/0/i915_edp_psr_status
    
    Signed-off-by: George D Sworo <george.d.sworo@intel.com>
    Change-Id: I58701ef6f40470df483793ca1a0d582da80259b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3238001
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/display/intel_ddi.c
+++ drivers/gpu/drm/i915/display/intel_ddi.c
@@ -3189,7 +3189,7 @@ static void intel_enable_ddi_dp(struct intel_atomic_state *state,
 	if (!dig_port->lspcon.active || dig_port->dp.has_hdmi_sink)
 		intel_dp_set_infoframes(encoder, true, crtc_state, conn_state);
 
-	intel_edp_drrs_enable(intel_dp, crtc_state);
+	intel_drrs_enable(intel_dp, crtc_state);
 
 	if (crtc_state->has_audio)
 		intel_audio_codec_enable(encoder, crtc_state, conn_state);
@@ -3379,7 +3379,7 @@ static void intel_pre_disable_ddi(struct intel_atomic_state *state,
 		return;
 
 	intel_dp = enc_to_intel_dp(encoder);
-	intel_edp_drrs_disable(intel_dp, old_crtc_state);
+	intel_drrs_disable(intel_dp, old_crtc_state);
 	intel_psr_disable(intel_dp, old_crtc_state);
 }
 
@@ -3409,7 +3409,7 @@ static void intel_ddi_update_pipe_dp(struct intel_atomic_state *state,
 
 	intel_psr_update(intel_dp, crtc_state, conn_state);
 	intel_dp_set_infoframes(encoder, true, crtc_state, conn_state);
-	intel_edp_drrs_update(intel_dp, crtc_state);
+	intel_drrs_update(intel_dp, crtc_state);
 
 	intel_panel_update_backlight(state, encoder, crtc_state, conn_state);
 	drm_connector_update_privacy_screen(conn_state);
