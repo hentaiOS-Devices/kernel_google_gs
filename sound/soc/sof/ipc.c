d88b51d19938b6e9efd98ca11b0266eff8e4621b
Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
2021-09-17 11:58:22 +0300

    UPSTREAM: ASoC: SOF: ipc: Clarify the parameter name for ipc_trace_message()
    ipc_trace_message() receives the type not the ID.
    Use the same naming as the ipc_stream_message() function: msg_type to
    help the reader to follow the code.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Reviewed-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
    Reviewed-by: Bard Liao <bard.liao@intel.com>
    Link: https://lore.kernel.org/r/20210917085823.27222-2-peter.ujfalusi@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 59fdde1d4e268dbb9df5df77a7569c7d987607b6)
    
    BUG=b:218766331
    TEST=Test sound cards are listed and audio playback works.
    
    Signed-off-by: Terry Cheong <htcheong@chromium.org>
    Signed-off-by: Madhulika Dolke <madhulika.dolke@intel.com>
    (cherry picked from commit 59fdde1d4e268dbb9df5df77a7569c7d987607b6)
    Change-Id: I0fa23fffd53b7e7019cfd3b1734b9d963b78f415

--- sound/soc/sof/ipc.c
+++ sound/soc/sof/ipc.c
@@ -18,7 +18,7 @@
 #include "sof-audio.h"
 #include "ops.h"
 
-static void ipc_trace_message(struct snd_sof_dev *sdev, u32 msg_id);
+static void ipc_trace_message(struct snd_sof_dev *sdev, u32 msg_type);
 static void ipc_stream_message(struct snd_sof_dev *sdev, u32 msg_cmd);
 
 /*
@@ -481,12 +481,12 @@ EXPORT_SYMBOL(snd_sof_ipc_msgs_rx);
  * IPC trace mechanism.
  */
 
-static void ipc_trace_message(struct snd_sof_dev *sdev, u32 msg_id)
+static void ipc_trace_message(struct snd_sof_dev *sdev, u32 msg_type)
 {
 	struct sof_ipc_dma_trace_posn posn;
 	int ret;
 
-	switch (msg_id) {
+	switch (msg_type) {
 	case SOF_IPC_TRACE_DMA_POSITION:
 		/* read back full message */
 		ret = snd_sof_ipc_msg_data(sdev, NULL, &posn, sizeof(posn));
@@ -496,8 +496,7 @@ static void ipc_trace_message(struct snd_sof_dev *sdev, u32 msg_id)
 			snd_sof_trace_update_pos(sdev, &posn);
 		break;
 	default:
-		dev_err(sdev->dev, "error: unhandled trace message %x\n",
-			msg_id);
+		dev_err(sdev->dev, "error: unhandled trace message %x\n", msg_type);
 		break;
 	}
 }
