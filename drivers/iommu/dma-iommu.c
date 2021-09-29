06929a35e7576b6bb2d5625090d88fb3fd8de0b1
David Stevens <stevensd@chromium.org>
2021-09-29 11:32:56 +0900

    FROMGIT: iommu/dma: Skip extra sync during unmap w/swiotlb
    Calling the iommu_dma_sync_*_for_cpu functions during unmap can cause
    two copies out of the swiotlb buffer. Do the arch sync directly in
    __iommu_dma_unmap_swiotlb instead to avoid this. This makes the call to
    iommu_dma_sync_sg_for_cpu for untrusted devices in iommu_dma_unmap_sg no
    longer necessary, so move that invocation later in the function.
    
    Signed-off-by: David Stevens <stevensd@chromium.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>
    Link: https://lore.kernel.org/r/20210929023300.335969-4-stevensd@google.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    (cherry picked from commit ee9d4097cc145dcaebedf6b113d17c91c21333a0
     git://git.kernel.org/pub/scm/linux/kernel/git/joro/iommu.git core)
    
    BUG=b:167731151, b:200576547
    TEST=TEST=1)Build and boot on Volteer/Brya and do not see any new errors.
         2)Test data transfer on an external thunderbolt NVME (untrusted
         device) on volteer.
    
    Signed-off-by: Rajat Jain <rajatja@google.com>
    Change-Id: Idf0c3cc3a7681a3f11b62e7adf4a3ff9a868a5f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3134507
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: David Stevens <stevensd@chromium.org>
    Reviewed-by: Benson Leung <bleung@google.com>

--- drivers/iommu/dma-iommu.c
+++ drivers/iommu/dma-iommu.c
@@ -503,6 +503,9 @@ static void __iommu_dma_unmap_swiotlb(struct device *dev, dma_addr_t dma_addr,
 	if (WARN_ON(!phys))
 		return;
 
+	if (!(attrs & DMA_ATTR_SKIP_CPU_SYNC) && !dev_is_dma_coherent(dev))
+		arch_sync_dma_for_cpu(phys, size, dir);
+
 	__iommu_dma_unmap(dev, dma_addr, size);
 
 	if (unlikely(is_swiotlb_buffer(phys)))
@@ -860,8 +863,6 @@ static dma_addr_t iommu_dma_map_page(struct device *dev, struct page *page,
 static void iommu_dma_unmap_page(struct device *dev, dma_addr_t dma_handle,
 		size_t size, enum dma_data_direction dir, unsigned long attrs)
 {
-	if (!(attrs & DMA_ATTR_SKIP_CPU_SYNC))
-		iommu_dma_sync_single_for_cpu(dev, dma_handle, size, dir);
 	__iommu_dma_unmap_swiotlb(dev, dma_handle, size, dir, attrs);
 }
 
@@ -1069,13 +1070,13 @@ static void iommu_dma_unmap_sg(struct device *dev, struct scatterlist *sg,
 	struct scatterlist *tmp;
 	int i;
 
-	if (!(attrs & DMA_ATTR_SKIP_CPU_SYNC))
-		iommu_dma_sync_sg_for_cpu(dev, sg, nents, dir);
-
 	if (dev_is_untrusted(dev)) {
 		iommu_dma_unmap_sg_swiotlb(dev, sg, nents, dir, attrs);
 		return;
 	}
+
+	if (!(attrs & DMA_ATTR_SKIP_CPU_SYNC))
+		iommu_dma_sync_sg_for_cpu(dev, sg, nents, dir);
 
 	/*
 	 * The scatterlist segments are mapped into a single
