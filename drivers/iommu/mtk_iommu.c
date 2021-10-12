3f4e0007c92064d63b08732e5fe3508b2a0d00a3
Yong Wu <yong.wu@mediatek.com>
2021-10-12 20:41:14 +0800

    BACKPORT: FROMLIST: iommu/mediatek: Add device_link between the consumer and the larb devices
    MediaTek IOMMU-SMI diagram is like below. all the consumer connect with
    smi-larb, then connect with smi-common.
    
            M4U
             |
        smi-common
             |
      -------------
      |         |    ...
      |         |
    larb1     larb2
      |         |
    vdec       venc
    
    When the consumer works, it should enable the smi-larb's power which
    also need enable the smi-common's power firstly.
    
    Thus, First of all, use the device link connect the consumer and the
    smi-larbs. then add device link between the smi-larb and smi-common.
    
    This patch adds device_link between the consumer and the larbs.
    
    When device_link_add, I add the flag DL_FLAG_STATELESS to avoid calling
    pm_runtime_xx to keep the original status of clocks. It can avoid two
    issues:
    1) Display HW show fastlogo abnormally reported in [1]. At the beggining,
    all the clocks are enabled before entering kernel, but the clocks for
    display HW(always in larb0) will be gated after clk_enable and clk_disable
    called from device_link_add(->pm_runtime_resume) and rpm_idle. The clock
    operation happened before display driver probe. At that time, the
    display
    HW will be abnormal.
    
    2) A deadlock issue reported in [2]. Use DL_FLAG_STATELESS to skip
    pm_runtime_xx to avoid the deadlock.
    
    Corresponding, DL_FLAG_AUTOREMOVE_CONSUMER can't be added, then
    device_link_removed should be added explicitly.
    
    Meanwhile, Currently we don't have a device connect with 2 larbs at the
    same time. Disallow this case, print the error log.
    
    [1] https://lore.kernel.org/linux-mediatek/1564213888.22908.4.camel@mhfsdcap03/
    [2] https://lore.kernel.org/patchwork/patch/1086569/
    
    Suggested-by: Tomasz Figa <tfiga@chromium.org>
    Signed-off-by: Yong Wu <yong.wu@mediatek.com>
    Tested-by: Frank Wunderlich <frank-w@public-files.de> # BPI-R2/MT7623
    (am from https://patchwork.kernel.org/patch/12616563)
    (also found at https://lore.kernel.org/linux-mediatek/20211112105509.12010-6-yong.wu@mediatek.com)
    
    BUG=b:188609090
    TEST=Boot OK
    
    Change-Id: I549edcd98357fcd98f48c5f481ef6eb92e633689
    Signed-off-by: Yong Wu <yong.wu@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3218187
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Tested-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Commit-Queue: Tzung-Bi Shih <tzungbi@chromium.org>

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -652,7 +652,7 @@ static struct iommu_device *mtk_iommu_probe_device(struct device *dev)
 	struct mtk_iommu_data *data;
 	struct device_link *link;
 	struct device *larbdev;
-	unsigned int larbid;
+	unsigned int larbid, larbidx, i;
 
 	if (!fwspec || fwspec->ops != &mtk_iommu_ops)
 		return ERR_PTR(-ENODEV); /* Not a iommu client device */
@@ -666,6 +666,14 @@ static struct iommu_device *mtk_iommu_probe_device(struct device *dev)
 	 */
 	if (MTK_IOMMU_IS_TYPE(data->plat_data, MTK_IOMMU_TYPE_MM)) {
 		larbid = MTK_M4U_TO_LARB(fwspec->ids[0]);
+		for (i = 1; i < fwspec->num_ids; i++) {
+			larbidx = MTK_M4U_TO_LARB(fwspec->ids[i]);
+			if (larbid != larbidx) {
+				dev_err(dev, "Can only use one larb. Fail@larb%d-%d.\n",
+					larbid, larbidx);
+				return ERR_PTR(-EINVAL);
+			}
+		}
 		larbdev = data->larb_imu[larbid].dev;
 		link = device_link_add(dev, larbdev,
 				DL_FLAG_PM_RUNTIME | DL_FLAG_STATELESS);
