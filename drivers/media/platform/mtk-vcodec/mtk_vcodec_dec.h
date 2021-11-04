2498d90f9f986a6dd466202f56fec8eeb451a86a
Yunfei Dong <yunfei.dong@mediatek.com>
2021-11-04 12:24:26 +0000

    FROMGIT: media: mtk-vcodec: Align width and height to 64 bytes
    Width and height need to 64 bytes aligned when setting the format.
    Need to make sure all is 64 bytes align when use width and height to
    calculate buffer size.
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    Acked-by: Nicolas Dufresne <nicolas.dufresne@collabora.com>
    Tested-by: Steve Cho <stevecho@chromium.org>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    (cherry picked from commit b5150b6ec1cf45c2f499b501b6622a562437ee37
     git://linuxtv.org/media_stage.git master)
    
    BUG=b:149068426
    TEST=vp8 decoding works on Krane
    
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
    Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Change-Id: Ia9f3a426a6bf0c40b74bec41464ef0162a91aa1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3291115
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@google.com>
    Tested-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    (cherry picked from commit 2850935b30ee8a38002c4cf94301daaf5057c340)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3312800
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Tzung-Bi Shih <tzungbi@chromium.org>

--- drivers/media/platform/mtk-vcodec/mtk_vcodec_dec.h
+++ drivers/media/platform/mtk-vcodec/mtk_vcodec_dec.h
@@ -14,6 +14,7 @@
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-mem2mem.h>
 
+#define VCODEC_DEC_ALIGNED_64 64
 #define VCODEC_CAPABILITY_4K_DISABLED	0x10
 #define VCODEC_DEC_4K_CODED_WIDTH	4096U
 #define VCODEC_DEC_4K_CODED_HEIGHT	2304U
