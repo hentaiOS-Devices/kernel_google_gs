ed473e7cb70607f86d9b8b88856097602308eb04
P Praneesh <ppranees@codeaurora.org>
2021-11-12 11:02:12 +0200

    UPSTREAM: ath11k: avoid additional access to ath11k_hal_srng_dst_num_free
    In ath11k_dp_process_rx(), after processing rx_desc from
    ath11k_hal_srng_dst_get_next_entry(), ath11k_hal_srng_dst_num_free()
    is accessed everytime because of done flag is not set.
    
    To avoid this additional access to ath11k_hal_srng_dst_num_free(),
    increment total_msdu_reaped only when continuation is not set and
    update done flag correspondingly.
    
    Tested-on: QCN9074 hw1.0 PCI WLAN.HK.2.4.0.1.r2-00012-QCAHKSWPL_SILICONZ-1
    Tested-on: IPQ8074 hw2.0 AHB WLAN.HK.2.4.0.1-01695-QCAHKSWPL_SILICONZ-1
    
    Co-developed-by: Sriram R <srirrama@codeaurora.org>
    Signed-off-by: Sriram R <srirrama@codeaurora.org>
    Signed-off-by: Jouni Malinen <jouni@codeaurora.org>
    Signed-off-by: P Praneesh <ppranees@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/1630560820-21905-5-git-send-email-ppranees@codeaurora.org
    (cherry picked from commit a1775e732eb90486519de3813b83a11b7fcee2d0)
    
    BUG=b:214319381
    TEST=Verified wifi
    
    Signed-off-by: Abhishek Kumar <kuabhs@chromium.org>
    Change-Id: I4095263b90ffc35a8c95da7ea7bf1d8857e24983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3391319
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Commit-Queue: Raul Rangel <rrangel@chromium.org>

--- drivers/net/wireless/ath/ath11k/dp_rx.c
+++ drivers/net/wireless/ath/ath11k/dp_rx.c
@@ -2644,7 +2644,6 @@ try_again:
 				 DMA_FROM_DEVICE);
 
 		num_buffs_reaped[mac_id]++;
-		total_msdu_reaped++;
 
 		push_reason = FIELD_GET(HAL_REO_DEST_RING_INFO0_PUSH_REASON,
 					desc.info0);
@@ -2667,10 +2666,15 @@ try_again:
 
 		__skb_queue_tail(&msdu_list, msdu);
 
-		if (total_msdu_reaped >= quota && !rxcb->is_continuation) {
+		if (rxcb->is_continuation) {
+			done = false;
+		} else {
+			total_msdu_reaped++;
 			done = true;
-			break;
 		}
+
+		if (total_msdu_reaped >= budget)
+			break;
 	}
 
 	/* Hw might have updated the head pointer after we cached it.
