9fd3ac6e53eedc4107260ec410036df3aeeeead8
Tejas Upadhyay <tejaskumarx.surendrakumar.upadhyay@intel.com>
2021-11-17 11:33:21 +0530

    BACKPORT: drm/i915/gt: Hold RPM wakelock during PXP suspend
    selftest --r live shows failure in suspend tests when
    RPM wakelock is not acquired during suspend.
    
    This changes addresses below error :
    <4> [154.177535] RPM wakelock ref not held during HW access
    <4> [154.177575] WARNING: CPU: 4 PID: 5772 at
    drivers/gpu/drm/i915/intel_runtime_pm.h:113
    fwtable_write32+0x240/0x320 [i915]
    <4> [154.177974] Modules linked in: i915(+) vgem drm_shmem_helper
    fuse snd_hda_codec_hdmi snd_hda_codec_realtek snd_hda_codec_generic
    ledtrig_audio mei_hdcp mei_pxp x86_pkg_temp_thermal coretemp
    crct10dif_pclmul crc32_pclmul ghash_clmulni_intel snd_intel_dspcfg
    snd_hda_codec snd_hwdep igc snd_hda_core ttm mei_me ptp
    snd_pcm prime_numbers mei i2c_i801 pps_core i2c_smbus intel_lpss_pci
    btusb btrtl btbcm btintel bluetooth ecdh_generic ecc [last unloaded: i915]
    <4> [154.178143] CPU: 4 PID: 5772 Comm: i915_selftest Tainted: G
    U            5.15.0-rc6-CI-Patchwork_21432+ #1
    <4> [154.178154] Hardware name: ASUS System Product Name/TUF GAMING
    Z590-PLUS WIFI, BIOS 0811 04/06/2021
    <4> [154.178160] RIP: 0010:fwtable_write32+0x240/0x320 [i915]
    <4> [154.178604] Code: 15 7b e1 0f 0b e9 34 fe ff ff 80 3d a9 89 31
    00 00 0f 85 31 fe ff ff 48 c7 c7 88 9e 4f a0 c6 05 95 89 31 00 01 e8
    c0 15 7b e1 <0f> 0b e9 17 fe ff ff 8b 05 0f 83 58 e2 85 c0 0f 85 8d
    00 00 00 48
    <4> [154.178614] RSP: 0018:ffffc900016279f0 EFLAGS: 00010286
    <4> [154.178626] RAX: 0000000000000000 RBX: ffff888204fe0ee0
    RCX: 0000000000000001
    <4> [154.178634] RDX: 0000000080000001 RSI: ffffffff823142b5
    RDI: 00000000ffffffff
    <4> [154.178641] RBP: 00000000000320f0 R08: 0000000000000000
    R09: c0000000ffffcd5a
    <4> [154.178647] R10: 00000000000f8c90 R11: ffffc90001627808
    R12: 0000000000000000
    <4> [154.178654] R13: 0000000040000000 R14: ffffffffa04d12e0
    R15: 0000000000000000
    <4> [154.178660] FS:  00007f7390aa4c00(0000) GS:ffff88844f000000(0000)
    knlGS:0000000000000000
    <4> [154.178669] CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
    <4> [154.178675] CR2: 000055bc40595028 CR3: 0000000204474005
    CR4: 0000000000770ee0
    <4> [154.178682] PKRU: 55555554
    <4> [154.178687] Call Trace:
    <4> [154.178706]  intel_pxp_fini_hw+0x23/0x30 [i915]
    <4> [154.179284]  intel_pxp_suspend+0x1f/0x30 [i915]
    <4> [154.179807]  live_gt_resume+0x5b/0x90 [i915]
    
    Changes since V2 :
    	- Remove boolean in intel_pxp_runtime_preapre for
    	  non-pxp configs. Solves build error
    Changes since V2 :
    	- Open-code intel_pxp_runtime_suspend - Daniele
    	- Remove boolean in intel_pxp_runtime_preapre - Daniele
    Changes since V1 :
    	- split the HW access parts in gt_suspend_late - Daniele
    	- Remove default PXP configs
    
    Signed-off-by: Tejas Upadhyay <tejaskumarx.surendrakumar.upadhyay@intel.com>
    Reviewed-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Fixes: 0cfab4cb3c4e ("drm/i915/pxp: Enable PXP power management")
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211117060321.3729343-1-tejaskumarx.surendrakumar.upadhyay@intel.com
    (cherry picked from commit d22d446f7a1ccd0db8c138749dde601388b2327d)
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    (cherry picked from commit b8d8436840caa2e9b6d156e69336d2135f49f10f)
    
    - Fix conflicts with intel_uc_suspend() moved around upstream
    - Fix conflicts with intel_pxp_invalidate() not used in CrOS
    
    BUG=b:208454744
    TEST=suspend_stress_test -c 1 during protected playback
    
    Signed-off-by: Juston Li <juston.li@intel.com>
    Change-Id: Iacd5a1dce675dea23003a4dd103c8de620d14646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3306564
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Tested-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>

--- drivers/gpu/drm/i915/gt/intel_gt_pm.c
+++ drivers/gpu/drm/i915/gt/intel_gt_pm.c
@@ -302,7 +302,7 @@ void intel_gt_suspend_prepare(struct intel_gt *gt)
 	user_forcewake(gt, true);
 	wait_for_suspend(gt);
 
-	intel_pxp_suspend(&gt->pxp, false);
+	intel_pxp_suspend_prepare(&gt->pxp);
 	intel_uc_suspend(&gt->uc);
 }
 
@@ -326,6 +326,8 @@ void intel_gt_suspend_late(struct intel_gt *gt)
 		return;
 
 	GEM_BUG_ON(gt->awake);
+
+	intel_pxp_suspend(&gt->pxp);
 
 	/*
 	 * On disabling the device, we want to turn off HW access to memory
@@ -353,7 +355,7 @@ void intel_gt_suspend_late(struct intel_gt *gt)
 
 void intel_gt_runtime_suspend(struct intel_gt *gt)
 {
-	intel_pxp_suspend(&gt->pxp, true);
+	intel_pxp_runtime_suspend(&gt->pxp);
 	intel_uc_runtime_suspend(&gt->uc);
 
 	GT_TRACE(gt, "\n");
@@ -371,7 +373,7 @@ int intel_gt_runtime_resume(struct intel_gt *gt)
 	if (ret)
 		return ret;
 
-	intel_pxp_resume(&gt->pxp);
+	intel_pxp_runtime_resume(&gt->pxp);
 
 	return 0;
 }
