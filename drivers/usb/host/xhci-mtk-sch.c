d909c77655a7652f78c10938ead0553cf7f28792
Tianping Fang <tianping.fang@mediatek.corp-partner.google.com>
2021-11-18 20:44:14 +0800

    CHROMIUM: Revert "UPSTREAM: usb: xhci-mtk: update fs bus bandwidth by bw_budget_table"
    Use the bw_cost_per_microframe to update the fs bus bandwidth
    
    This reverts commit e5e3725a317db06ba437dd2cbe867a38881b4026.
    
    Original commit description:
    > UPSTREAM: usb: xhci-mtk: update fs bus bandwidth by bw_budget_table
    >
    > Use @bw_budget_table[] to update fs bus bandwidth due to
    > not all microframes consume @bw_cost_per_microframe, see
    > setup_sch_info().
    >
    > Signed-off-by: Chunfeng Yun <chunfeng.yun@mediatek.com>
    > Link:
    > https://lore.kernel.org/r/1629189389-18779-6-git-send-email-chunfeng.yun@mediatek.com
    > Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    > (cherry picked from commit 451d3912586aad3f71f1c97780a1c21e3de98413)
    >
    > BUG=b:188609317
    > TEST=emerge-cherry sys-kernel/chromeos-kernel-5_10
    >
    > Signed-off-by: Fei Shao <fshao@chromium.org>
    > Change-Id: I3b7258a51ddd4916e5f8bfc11df5fb02739981f4
    > Reviewed-on:
    > https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3178204
    > Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    > Reviewed-by: Sean Paul <seanpaul@chromium.org>
    > Tested-by: Tzung-Bi Shih <tzungbi@chromium.org>
    
    BUG=b:199119575
    TEST=emerge-cherry sys-kernel/chromeos-kernel-5_10
    
    Signed-off-by: Tianping Fang <tianping.fang@mediatek.corp-partner.google.com>
    Change-Id: Ic41674eb546421873c18bddc78d641395b613cfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3295822
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Tested-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Commit-Queue: Tzung-Bi Shih <tzungbi@chromium.org>

--- drivers/usb/host/xhci-mtk-sch.c
+++ drivers/usb/host/xhci-mtk-sch.c
@@ -454,7 +454,7 @@ static int check_fs_bus_bw(struct mu3h_sch_ep_info *sch_ep, int offset)
 	struct mu3h_sch_tt *tt = sch_ep->sch_tt;
 	u32 tmp;
 	int base;
-	int i, j, k;
+	int i, j;
 
 	for (i = 0; i < sch_ep->num_esit; i++) {
 		base = offset + i * sch_ep->esit;
@@ -463,9 +463,8 @@ static int check_fs_bus_bw(struct mu3h_sch_ep_info *sch_ep, int offset)
 		 * Compared with hs bus, no matter what ep type,
 		 * the hub will always delay one uframe to send data
 		 */
-		for (j = 0; j < sch_ep->num_budget_microframes; j++) {
-			k = XHCI_MTK_BW_INDEX(base + j);
-			tmp = tt->fs_bus_bw[k] + sch_ep->bw_budget_table[j];
+		for (j = 0; j < sch_ep->cs_count; j++) {
+			tmp = tt->fs_bus_bw[base + j] + sch_ep->bw_cost_per_microframe;
 			if (tmp > FS_PAYLOAD_MAX)
 				return -ESCH_BW_OVERFLOW;
 		}
@@ -539,19 +538,22 @@ static int check_sch_tt(struct mu3h_sch_ep_info *sch_ep, u32 offset)
 static void update_sch_tt(struct mu3h_sch_ep_info *sch_ep, bool used)
 {
 	struct mu3h_sch_tt *tt = sch_ep->sch_tt;
-	u32 base;
-	int i, j, k;
+	u32 base, num_esit;
+	int bw_updated;
+	int i, j;
 
-	for (i = 0; i < sch_ep->num_esit; i++) {
+	num_esit = XHCI_MTK_MAX_ESIT / sch_ep->esit;
+
+	if (used)
+		bw_updated = sch_ep->bw_cost_per_microframe;
+	else
+		bw_updated = -sch_ep->bw_cost_per_microframe;
+
+	for (i = 0; i < num_esit; i++) {
 		base = sch_ep->offset + i * sch_ep->esit;
 
-		for (j = 0; j < sch_ep->num_budget_microframes; j++) {
-			k = XHCI_MTK_BW_INDEX(base + j);
-			if (used)
-				tt->fs_bus_bw[k] += sch_ep->bw_budget_table[j];
-			else
-				tt->fs_bus_bw[k] -= sch_ep->bw_budget_table[j];
-		}
+		for (j = 0; j < sch_ep->cs_count; j++)
+			tt->fs_bus_bw[base + j] += bw_updated;
 	}
 
 	if (used)
