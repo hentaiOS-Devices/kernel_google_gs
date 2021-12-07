cdf685dfe57ccd29069b88b045d73628c85008bd
Eliot Lee <eliot.lee@intel.corp-partner.google.com>
2021-12-07 11:30:10 -0800

    Revert "FIXUP: CHROMIUM: t7xx: Initial support for cldma0, sap, brom download port"
    This reverts commit 747b866a7b3fbecca9f0d7ce724dcf2b8a5071c1.
    
    BUG=b:197895466
    
    Signed-off-by: Eliot Lee <eliot.lee@intel.corp-partner.google.com>
    Change-Id: Icfd592b680247b899f50af7888e7db07826a93f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3321823
    Tested-by: Daniel Winkler <danielwinkler@google.com>
    Reviewed-by: Daniel Winkler <danielwinkler@google.com>
    Commit-Queue: Daniel Winkler <danielwinkler@google.com>

--- drivers/net/wwan/t7xx/t7xx_modem_ops.c
+++ drivers/net/wwan/t7xx/t7xx_modem_ops.c
@@ -578,7 +578,7 @@ exit:
 	kfree(event);
 }
 
-static void sap_hk_wq(struct work_struct *work)
+void sap_hk_wq(struct work_struct *work)
 {
 	struct mtk_modem *md = container_of(work, struct mtk_modem, sap_handshake_work);
         struct core_sys_info *core_info;
