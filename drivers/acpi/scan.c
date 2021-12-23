01728466e773fb305fbe1898133b27d0c5c89a13
Heikki Krogerus <heikki.krogerus@linux.intel.com>
2021-12-23 11:16:17 +0300

    BACKPORT: FROMGIT: acpi: Store CRC-32 hash of the _PLD in struct acpi_device
    Storing CRC-32 hash of the Physical Location of Device
    object (_PLD) with devices that have it. The hash is stored
    to a new struct acpi_device member "pld_crc".
    
    The hash makes it easier to find devices that share a
    location, as there is no need to evaluate the entire object
    every time. Knowledge about devices that share a location
    can be used in device drivers that need to know the
    connections to other components inside a system. USB3 ports
    will for example always share their location with a USB2
    port.
    
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Link: https://lore.kernel.org/r/20211223081620.45479-3-heikki.krogerus@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit 882c982dada4d53079c56de94ccbce1e21cc675f
     git://git.kernel.org/pub/scm/linux/kernel/git/gregkh/usb.git usb-next)
    
    Conflicts:
       drivers/acpi/scan.c
    
    BUG=b:207493740
    TEST=Build and boot on brya; verify USB devices are connected to ports
    
    Signed-off-by: Prashant Malani <pmalani@chromium.org>
    Change-Id: I872c601125680d14a34866082d47f474b347fa55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3379045
    Reviewed-by: Rajat Jain <rajatja@google.com>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -18,6 +18,7 @@
 #include <linux/dma-map-ops.h>
 #include <linux/platform_data/x86/apple.h>
 #include <linux/pgtable.h>
+#include <linux/crc32.h>
 
 #include "internal.h"
 
@@ -650,6 +651,18 @@ static int acpi_device_set_name(struct acpi_device *device,
 	dev_set_name(&device->dev, "%s:%02x", acpi_device_bus_id->bus_id, result);
 	return 0;
 }
+static void acpi_store_pld_crc(struct acpi_device *adev)
+{
+	struct acpi_pld_info *pld;
+	acpi_status status;
+
+	status = acpi_get_physical_device_location(adev->handle, &pld);
+	if (ACPI_FAILURE(status))
+		return;
+
+	adev->pld_crc = crc32(~0, pld, sizeof(*pld));
+	ACPI_FREE(pld);
+}
 
 int acpi_device_add(struct acpi_device *device,
 		    void (*release)(struct device *))
@@ -720,6 +733,9 @@ int acpi_device_add(struct acpi_device *device,
 
 	if (device->wakeup.flags.valid)
 		list_add_tail(&device->wakeup_list, &acpi_wakeup_device_list);
+
+	acpi_store_pld_crc(device);
+
 	mutex_unlock(&acpi_device_lock);
 
 	if (device->parent)
