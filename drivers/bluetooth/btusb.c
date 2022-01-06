9d5c1d80365c9d656bb433690b2844a5eb308daf
Radoslaw Biernacki <rad@semihalf.com>
2022-01-06 22:45:05 +0000

    FROMLIST: Bluetooth: fix 5.15 init regression for Intel Bluetooth
    Fixes the boot regression against 5.14:
     Bluetooth: hci0: Reading Intel version command failed (-110)
     Bluetooth: hci0: command tx timeout
    
    Fixes: 83f2dafe2a62 ("Bluetooth: btintel: Refactoring setup routine for legacy ROM sku")
    Buglink: https://bugzilla.opensuse.org/show_bug.cgi?id=1193124
    
    Signed-off-by: Ortwin Glück <odi@odi.ch>
    (am from https://patchwork.kernel.org/patch/12699683/)
    
    BUG=b:207769979,b:213330295
    TEST=Build, flash kernel
    TEST=Check bluetooth works on coral-kernelnext and
         CloudReady2.0 on x1 carbon gen 8 and x1 carbon gen 9
    
    Signed-off-by: Marek Maslanka <mm@semihalf.com>
    Signed-off-by: Radoslaw Biernacki <rad@semihalf.com>
    Signed-off-by: Nicholas Bishop<nicholasbishop@google.com>
    Change-Id: I452abd72df424ab7b6044226d1ffb17ab1195761
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3370164
    Reviewed-by: Marek Maślanka <mmaslanka@google.com>
    Reviewed-by: Will Smith <wpsmith@google.com>
    Tested-by: Radoslaw Biernacki <biernacki@google.com>
    Commit-Queue: Radoslaw Biernacki <biernacki@google.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -366,7 +366,8 @@ static const struct usb_device_id blacklist_table[] = {
 	{ USB_DEVICE(0x8087, 0x07da), .driver_info = BTUSB_CSR },
 	{ USB_DEVICE(0x8087, 0x07dc), .driver_info = BTUSB_INTEL_COMBINED |
 						     BTUSB_INTEL_BROKEN_INITIAL_NCMD },
-	{ USB_DEVICE(0x8087, 0x0a2a), .driver_info = BTUSB_INTEL_COMBINED },
+	{ USB_DEVICE(0x8087, 0x0a2a), .driver_info = BTUSB_INTEL_COMBINED |
+						     BTUSB_INTEL_BROKEN_INITIAL_NCMD },
 	{ USB_DEVICE(0x8087, 0x0a2b), .driver_info = BTUSB_INTEL_COMBINED },
 	{ USB_DEVICE(0x8087, 0x0aa7), .driver_info = BTUSB_INTEL_COMBINED },
 	{ USB_DEVICE(0x8087, 0x0aaa), .driver_info = BTUSB_INTEL_COMBINED },
