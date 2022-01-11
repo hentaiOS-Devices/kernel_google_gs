c65b9532e20963565a11491f9bfdb5bdba9e40a1
Lorenzo Bianconi <lorenzo@kernel.org>
2022-01-11 10:34:17 +0100

    BACKPORT: FROMLIST: mt76: mt7921: toggle runtime-pm adding a monitor vif
    Toggle runtime-pm and deep-sleep configuration adding/removing
    a montior vif in order to forward all tx/rx frames to mac80211.
    
    Conflicts: a conflict happened at
    	   drivers/net/wireless/mediatek/mt76/mt7921/init.c since we did the
    	   backport with the commit 3225d2f3f131
    	   ("BACKPORT: FROMLIST: mt76: mt7921s: run sleep mode by default")
    	   because at that time we didn't include the patch
    	   ("mt76: mt7921: toggle runtime-pm adding a monitor vif")
    
    Backport Note:
    	   adjusted again by the context to be aligned with the upstream dirver
    	   becasue right now we have included the patch
               ("mt76: mt7921: toggle runtime-pm adding a monitor vif")
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Tested-by: Sven Eckelmann <sven@narfation.org>
    (am from https://patchwork.kernel.org/patch/12709654/)
    (also found at https://lore.kernel.org/r/f6ad709b1a64152b5955e71b7eef9f9c4c7f127e.1641893305.git.lorenzo@kernel.org)
    
    BUG=b:178754244
    TEST=build on cherry
    
    Signed-off-by: Sean Wang <objelf@gmail.com>
    Signed-off-by: Abhishek Kumar <kuabhs@chromium.org>
    Change-Id: I707b0a541c3940c77b1bcfeaf7e4d3ce253e020d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3519900
    Reviewed-by: sean wang <keyhaede@gmail.com>
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>

--- drivers/net/wireless/mediatek/mt76/mt7921/init.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/init.c
@@ -230,7 +230,9 @@ int mt7921_register_device(struct mt7921_dev *dev)
 	dev->pm.idle_timeout = MT7921_PM_TIMEOUT;
 	dev->pm.stats.last_wake_event = jiffies;
 	dev->pm.stats.last_doze_event = jiffies;
+	dev->pm.enable_user = true;
 	dev->pm.enable = true;
+	dev->pm.ds_enable_user = true;
 	dev->pm.ds_enable = true;
 
 	if (mt76_is_sdio(&dev->mt76))
