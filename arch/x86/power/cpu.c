6ba7b9b73807270549f9d6d95ccc18e7df95255b
Chang S. Bae <chang.seok.bae@intel.com>
2022-01-12 13:12:54 -0800

    FROMLIST: x86/PM/keylocker: Restore internal wrapping key on resume from ACPI S3/4
    When the system enters the ACPI S3 or S4 sleep states, the internal
    wrapping key is discarded.
    
    The primary use case for the feature is bare metal dm-crypt. The key needs
    to be restored properly on wakeup, as dm-crypt does not prompt for the key
    on resume from suspend. Even the prompt it does perform for unlocking
    the volume where the hibernation image is stored, it still expects to reuse
    the key handles within the hibernation image once it is loaded. So it is
    motivated to meet dm-crypt's expectation that the key handles in the
    suspend-image remain valid after resume from an S-state.
    
    Key Locker provides a mechanism to back up the internal wrapping key in
    non-volatile storage. The kernel requests a backup right after the key is
    loaded at boot time. It is copied back to each CPU upon wakeup.
    
    While the backup may be maintained in NVM across S5 and G3 "off"
    states it is not architecturally guaranteed, nor is it expected by dm-crypt
    which expects to prompt for the key each time the volume is started.
    
    The entirety of Key Locker needs to be disabled if the backup mechanism is
    not available unless CONFIG_SUSPEND=n, otherwise dm-crypt requires the
    backup to be available.
    
    In the event of a key restore failure the kernel proceeds with an
    initialized IWKey state. This has the effect of invalidating any key
    handles that might be present in a suspend-image. When this happens
    dm-crypt will see I/O errors resulting from error returns from
    crypto_skcipher_{en,de}crypt(). While this will disrupt operations in the
    current boot, data is not at risk and access is restored at the next reboot
    to create new handles relative to the current IWKey.
    
    Manage a feature-specific flag to communicate with the crypto
    implementation. This ensures to stop using the AES instructions upon the
    key restore failure while not turning off the feature.
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: x86@kernel.org
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-pm@vger.kernel.org
    (am from https://patchwork.kernel.org/project/linux-crypto/patch/20220112211258.21115-9-chang.seok.bae@intel.com/)
    
    BUG=b:198473496
    TEST=compilation, tcrypt tests
    
    Signed-off-by: Sarthak Kukreti <sarthakkukreti@chromium.org>
    Change-Id: I1fd1e0953977e7958d7c529ec0eda167f5dabca2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3373770
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- arch/x86/power/cpu.c
+++ arch/x86/power/cpu.c
@@ -25,6 +25,7 @@
 #include <asm/cpu.h>
 #include <asm/mmu_context.h>
 #include <asm/cpu_device_id.h>
+#include <asm/keylocker.h>
 
 #ifdef CONFIG_X86_32
 __visible unsigned long saved_context_ebx;
@@ -265,6 +266,7 @@ static void notrace __restore_processor_state(struct saved_context *ctxt)
 	mtrr_bp_restore();
 	perf_restore_debug_store();
 	msr_restore_context(ctxt);
+	restore_keylocker();
 
 	c = &cpu_data(smp_processor_id());
 	if (cpu_has(c, X86_FEATURE_MSR_IA32_FEAT_CTL))
