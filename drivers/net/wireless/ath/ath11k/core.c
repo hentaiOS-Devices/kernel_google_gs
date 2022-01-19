e3a53680fb323b9f61b4c172b0049f59faa0c128
Wen Gong <quic_wgong@quicinc.com>
2022-01-18 23:13:55 -0500

    FROMLIST: ath11k: avoid firmware crash when reg set for QCA6390/WCN6855
    For the NL80211_REGDOM_SET_BY_USER hint from cfg80211, it set the new
    alpha2 code to ath11k, then ath11k send WMI_SET_INIT_COUNTRY_CMDID to
    firmware for all chips currently. When test with WCN6855/QCA6390 chips,
    this WMI CMD leads firmware crash.
    
    For AP based chips(ipq8074/qcn9074), WMI_SET_INIT_COUNTRY_CMDID is used
    to send to firmware, for STATION based chips(WCN6855/QCA6390), it need to
    use another WMI CMD WMI_SET_CURRENT_COUNTRY_CMDID.
    
    Add flag current_cc_support in hardware parameters, it is used to
    distinguish AP/STA platform. After that, the firmware will work
    normal and the regulatory feature works well for QCA6390/WCN6855.
    
    Tested-on: QCA6390 hw2.0 PCI WLAN.HST.1.0.1-01740-QCAHSTSWPLZ_V2_TO_X86-1
    Tested-on: WCN6855 hw2.0 PCI WLAN.HSP.1.1-03003-QCAHSPSWPL_V1_V2_SILICONZ_LITE-2
    
    Signed-off-by: Wen Gong <quic_wgong@quicinc.com>
    (am from https://patchwork.kernel.org/project/linux-wireless/patch/20220119041355.32014-1-quic_wgong@quicinc.com/)
    (also found at https://lore.kernel.org/r/20220119041355.32014-1-quic_wgong@quicinc.com)
    
    BUG=b:216658662
    TEST=Verified wifi
    
    Signed-off-by: Abhishek Kumar <kuabhs@chromium.org>
    [kuabhs: Rebased and fixed conflicts]
    Change-Id: Ifc22a42420a628e0f7ec530f035a3a45e91df60c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3422905
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

--- drivers/net/wireless/ath/ath11k/core.c
+++ drivers/net/wireless/ath/ath11k/core.c
@@ -97,6 +97,7 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.wakeup_mhi = false,
 		.supports_rssi_stats = false,
 		.fw_wmi_diag_event = false,
+		.current_cc_support = false,
 	},
 	{
 		.hw_rev = ATH11K_HW_IPQ6018_HW10,
@@ -161,6 +162,7 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.wakeup_mhi = false,
 		.supports_rssi_stats = false,
 		.fw_wmi_diag_event = false,
+		.current_cc_support = false,
 	},
 	{
 		.name = "qca6390 hw2.0",
@@ -224,6 +226,7 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.wakeup_mhi = true,
 		.supports_rssi_stats = true,
 		.fw_wmi_diag_event = true,
+		.current_cc_support = true,
 	},
 	{
 		.name = "qcn9074 hw1.0",
@@ -287,6 +290,7 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.wakeup_mhi = false,
 		.supports_rssi_stats = false,
 		.fw_wmi_diag_event = false,
+		.current_cc_support = false,
 	},
 	{
 		.name = "wcn6855 hw2.0",
@@ -350,6 +354,7 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.wakeup_mhi = true,
 		.supports_rssi_stats = true,
 		.fw_wmi_diag_event = true,
+		.current_cc_support = true,
 	},
 	{
 		.name = "wcn6855 hw2.1",
@@ -416,6 +421,7 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.wakeup_mhi = true,
 		.supports_rssi_stats = true,
 		.fw_wmi_diag_event = true,
+		.current_cc_support = true,
 	},
 };
 
