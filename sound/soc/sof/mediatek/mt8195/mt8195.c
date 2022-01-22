4fc07c87413c126e4a0b8cccfadac80275a1dfb3
YC Hung <yc.hung@mediatek.corp-partner.google.com>
2022-01-22 22:21:54 +0800

    CHROMIUM: ASoC: SOF: mediatek: Add mediatek debug dump
    1.Add mtk-adsp-common.c file for mediatek platforms common usage.
    2.Add mtk_adsp_dump implementation in mtk-adsp-common.c for general
      debug dump.
    3.Add mt8195_adsp_dump in mt8195.c for debug_dump callback to dump
      mt8195 debug registers and call mtk_adsp_dump.
    
    BUG=b:189238342
    TEST=emerge-cherry sys-kernel/chromeos-kernel-5_10
    
    Signed-off-by: YC Hung <yc.hung@mediatek.corp-partner.google.com>
    Change-Id: I2c42bec36b57399fe67af09ebdc1ef2316e1c369
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3410431
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>

--- sound/soc/sof/mediatek/mt8195/mt8195.c
+++ sound/soc/sof/mediatek/mt8195/mt8195.c
@@ -26,6 +26,7 @@
 #include "../adsp_helper.h"
 #include "../adsp-pcm.h"
 #include "../mediatek-ops.h"
+#include "../mtk-adsp-common.h"
 #include "mt8195.h"
 #include "mt8195-clk.h"
 
@@ -564,6 +565,33 @@ static int mt8195_ipc_pcm_params(struct snd_sof_dev *sdev,
 	return 0;
 }
 
+static void mt8195_adsp_dump(struct snd_sof_dev *sdev, u32 flags)
+{
+	u32 dbg_pc, dbg_data, dbg_bus0, dbg_bus1, dbg_inst;
+	u32 dbg_ls0stat, dbg_ls1stat, faultbus, faultinfo;
+	u32 swreset;
+
+	/* dump debug registers */
+	dbg_pc = snd_sof_dsp_read(sdev, DSP_REG_BAR, DSP_PDEBUGPC);
+	dbg_data = snd_sof_dsp_read(sdev, DSP_REG_BAR, DSP_PDEBUGDATA);
+	dbg_bus0 = snd_sof_dsp_read(sdev, DSP_REG_BAR, DSP_PDEBUGBUS0);
+	dbg_bus1 = snd_sof_dsp_read(sdev, DSP_REG_BAR, DSP_PDEBUGBUS1);
+	dbg_inst = snd_sof_dsp_read(sdev, DSP_REG_BAR, DSP_PDEBUGINST);
+	dbg_ls0stat = snd_sof_dsp_read(sdev, DSP_REG_BAR, DSP_PDEBUGLS0STAT);
+	dbg_ls1stat = snd_sof_dsp_read(sdev, DSP_REG_BAR, DSP_PDEBUGLS1STAT);
+	faultbus = snd_sof_dsp_read(sdev, DSP_REG_BAR, DSP_PFAULTBUS);
+	faultinfo = snd_sof_dsp_read(sdev, DSP_REG_BAR, DSP_PFAULTINFO);
+	swreset = snd_sof_dsp_read(sdev, DSP_REG_BAR, DSP_RESET_SW);
+
+	dev_info(sdev->dev, "adsp dump : pc 0x%x, data 0x%x, bus0 0x%x, bus1 0x%x, swrest 0x%x",
+		 dbg_pc, dbg_data, dbg_bus0, dbg_bus1, swreset);
+	dev_info(sdev->dev, "ls0stat 0x%x, ls1stat 0x%x, faultbus 0x%x, faultinfo 0x%x",
+		 dbg_ls0stat, dbg_ls1stat, faultbus, faultinfo);
+
+	mtk_adsp_dump(sdev, flags);
+}
+
+
 static struct snd_soc_dai_driver mt8195_dai[] = {
 {
 	.name = "SOF_DL2",
@@ -643,6 +671,9 @@ const struct snd_sof_dsp_ops sof_mt8195_ops = {
 
 	/* Firmware ops */
 	.dsp_arch_ops = &sof_xtensa_arch_ops,
+
+	/* Debug information */
+	.dbg_dump = mt8195_adsp_dump,
 
 	/* DAI drivers */
 	.drv = mt8195_dai,
