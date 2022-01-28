aa247b67b2e6f4300dbcce5870e5dfcbb56d810e
Yunfei Dong <yunfei.dong@mediatek.com>
2022-01-28 11:23:30 +0800

    BACKPORT: FROMGIT: media: uapi: Init VP9 stateless decode params
    Init some of VP9 frame decode params to default value.
    
    Fixes: b88dbe38dca8 ("media: uapi: Add VP9 stateless decoder controls")
    Signed-off-by: Yunfei Dong <yunfei.dong@mediatek.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    
    (cherry picked from commit f15799b7b796dfaea63a86913246202d64762941
     git://linuxtv.org/media_stage.git master)
    
    Conflicts:
       drivers/media/v4l2-core/v4l2-ctrls-core.c
    
    Backport: applied changes to v4l2-ctrls.c instead.
    
    BUG=b:214332361
    TEST=Build for kevin and cherry
    
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>
    Change-Id: Id0036c86f4497b9d7e4dc806daed1b9a9621cc2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3451595
    Reviewed-by: Yunfei Dong <yunfei.dong@mediatek.corp-partner.google.com>
    Reviewed-by: Miguel Casas <mcasas@google.com>
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Commit-Queue: Brian Norris <briannorris@chromium.org>

--- drivers/media/v4l2-core/v4l2-ctrls.c
+++ drivers/media/v4l2-core/v4l2-ctrls.c
@@ -1696,6 +1696,7 @@ static void std_init_compound(const struct v4l2_ctrl *ctrl, u32 idx,
 {
 	struct v4l2_ctrl_mpeg2_slice_params *p_mpeg2_slice_params;
 	struct v4l2_ctrl_vp8_frame *p_vp8_frame;
+	struct v4l2_ctrl_vp9_frame *p_vp9_frame;
 	struct v4l2_ctrl_fwht_params *p_fwht_params;
 	void *p = ptr.p + idx * ctrl->elem_size;
 
@@ -1722,6 +1723,13 @@ static void std_init_compound(const struct v4l2_ctrl *ctrl, u32 idx,
 	case V4L2_CTRL_TYPE_VP8_FRAME:
 		p_vp8_frame = p;
 		p_vp8_frame->num_dct_parts = 1;
+		break;
+	case V4L2_CTRL_TYPE_VP9_FRAME:
+		p_vp9_frame = p;
+		p_vp9_frame->profile = 0;
+		p_vp9_frame->bit_depth = 8;
+		p_vp9_frame->flags |= V4L2_VP9_FRAME_FLAG_X_SUBSAMPLING |
+			V4L2_VP9_FRAME_FLAG_Y_SUBSAMPLING;
 		break;
 	case V4L2_CTRL_TYPE_FWHT_PARAMS:
 		p_fwht_params = p;
