f7f52cc30ed7a6156a060b71930d7e302b0bf110
Ville Syrjälä <ville.syrjala@linux.intel.com>
2022-02-14 11:18:09 +0200

    UPSTREAM: drm/i915: Unconfuse pre-icl vs. icl+ intel_sagv_{pre,post}_plane_update()
    intel_sagv_{pre,post}_plane_update() can accidentally forget
    to bail out early on pre-icl and proceed down the icl+ codepath
    at the end of the function. Fortunately it'll bail out before
    it gets too far due to old_qgv_mask==new_qgv_mask==0 so no real
    bug here. But lets make the code less confusing anyway.
    
    Cc: Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220214091811.13725-5-ville.syrjala@linux.intel.com
    Reviewed-by: Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>
    (cherry picked from commit a19c1d00b0d350fb7b75c6c4da91887f66114a7d)
    
    BUG=b:210697438
    TEST=Tested on taniks, ensured seamless refresh update
    
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Change-Id: Id3e70d0efaa41d3efd5e39fc8cae436d183e95ec

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -3816,8 +3816,9 @@ void intel_sagv_pre_plane_update(struct intel_atomic_state *state)
 	if (!new_bw_state)
 		return;
 
-	if (DISPLAY_VER(dev_priv) < 11 && !intel_can_enable_sagv(dev_priv, new_bw_state)) {
-		intel_disable_sagv(dev_priv);
+	if (DISPLAY_VER(dev_priv) < 11) {
+		if (!intel_can_enable_sagv(dev_priv, new_bw_state))
+			intel_disable_sagv(dev_priv);
 		return;
 	}
 
@@ -3867,8 +3868,9 @@ void intel_sagv_post_plane_update(struct intel_atomic_state *state)
 	if (!new_bw_state)
 		return;
 
-	if (DISPLAY_VER(dev_priv) < 11 && intel_can_enable_sagv(dev_priv, new_bw_state)) {
-		intel_enable_sagv(dev_priv);
+	if (DISPLAY_VER(dev_priv) < 11) {
+		if (intel_can_enable_sagv(dev_priv, new_bw_state))
+			intel_enable_sagv(dev_priv);
 		return;
 	}
 
