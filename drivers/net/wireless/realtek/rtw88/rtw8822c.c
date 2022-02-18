9ff511b44c393074d31f41c0bad2e72e21ce62b6
Ping-Ke Shih <pkshih@realtek.com>
2022-02-18 11:55:27 +0800

    BACKPORT: rtw88: change rtw_info() to proper message level
    Larry reported funny log entries [1] when he used rtl8821ce. These
    messages are not harmless, but not useful for users, so change them to
    rtw_dbg() level. By the way, I review all rtw_info() and change others
    to rtw_warn().
    
    [1] https://lore.kernel.org/linux-wireless/c356d5ae-a7b3-3065-1121-64c446e70333@lwfinger.net/
    
    Reported-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20220218035527.9835-1-pkshih@realtek.com
    (cherry picked from commit a0061be4e54b52e5e4ff179c3f817107ddbb2830)
    
    Conflicts:
       drivers/net/wireless/realtek/rtw88/rtw8821c.c
       (* Unmerged path)
       drivers/net/wireless/realtek/rtw88/sar.c
       (* Unmerged path)
    
    BUG=b:279092049
    TEST=verify the following on Zork with RTL8822CE
         * suite:wifi_matfunc
         * suite:wifi_perf
         * wifi.SAPSimpleConnect
    
    Change-Id: I17580321ca8fa1e532a43cb2d48c7f896af06688
    Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4488477
    Reviewed-by: Jintao Lin <jintaolin@chromium.org>
    Tested-by: Jintao Lin <jintaolin@chromium.org>
    Commit-Queue: Jintao Lin <jintaolin@chromium.org>

--- drivers/net/wireless/realtek/rtw88/rtw8822c.c
+++ drivers/net/wireless/realtek/rtw88/rtw8822c.c
@@ -2795,7 +2795,7 @@ static int rtw8822c_set_antenna(struct rtw_dev *rtwdev,
 	case BB_PATH_AB:
 		break;
 	default:
-		rtw_info(rtwdev, "unsupported tx path 0x%x\n", antenna_tx);
+		rtw_warn(rtwdev, "unsupported tx path 0x%x\n", antenna_tx);
 		return -EINVAL;
 	}
 
@@ -2805,7 +2805,7 @@ static int rtw8822c_set_antenna(struct rtw_dev *rtwdev,
 	case BB_PATH_AB:
 		break;
 	default:
-		rtw_info(rtwdev, "unsupported rx path 0x%x\n", antenna_rx);
+		rtw_warn(rtwdev, "unsupported rx path 0x%x\n", antenna_rx);
 		return -EINVAL;
 	}
 
