4327f0c43cb0ebe1b161f982d51793682be65d3e
Ville Syrjälä <ville.syrjala@linux.intel.com>
2022-02-23 15:13:13 +0200

    BACKPORT: drm/i915: Eliminate bigjoiner boolean
    Since we now have the bigjoiner_pipes bitmask the boolean
    is redundant. Get rid of it.
    
    Also, populating bigjoiner_pipes already during
    encoder->compute_config() allows us to use it much earlier
    during the state calculation as well. The initial aim is
    to use it in intel_crtc_compute_config().
    
    v2: Move the hweight(bigjoiner_pipes) stuff to a later patch
    
    Reviewed-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com> #v1
    Reviewed-by: Manasi Navare <manasi.d.navare@intel.com> #v1
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220223131315.18016-12-ville.syrjala@linux.intel.com
    
    (cherry picked from commit 4d1b53dfbf2445a7cf35ca18c90c2c6712fea163)
    
    Conflicts:
       drivers/gpu/drm/i915/display/intel_cursor.c
    
    Downstream changes:
    -Conflicts with downstream patch a9725220a529 ("BACKPORT:
     drm/i915/display/psr2: Fix cursor updates using legacy apis")
    
    BUG=b:210697438
    TEST=Tested on taniks, ensured seamless refresh update
    
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Change-Id: I7ce9365d5c5724d33a934fc9a566404568a1efd5

--- drivers/gpu/drm/i915/display/intel_display_types.h
+++ drivers/gpu/drm/i915/display/intel_display_types.h
@@ -1202,9 +1202,6 @@ struct intel_crtc_state {
 	/* enable pipe csc? */
 	bool csc_enable;
 
-	/* enable pipe big joiner? */
-	bool bigjoiner;
-
 	/* big joiner pipe bitmask */
 	u8 bigjoiner_pipes;
 
