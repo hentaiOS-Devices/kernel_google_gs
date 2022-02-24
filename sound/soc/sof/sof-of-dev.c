c0a1803fa89581d3af37cd09d0308840e1c3232c
YC Hung <yc.hung@mediatek.corp-partner.google.com>
2022-02-24 10:11:09 +0800

    CHROMIUM: ASoC: SOF: mediatek: mt8195 change ipc timeout as 1 second
    DSP ipc trasmission is over than default 500ms during platform boot
    flow then trigger exception. Check the time between DSP send ipc complete
    notification via mailbox interrupt and linux receive and handle the irq
    in the function "mt8195_dsp_handle_reply". Generally it takes more than
    500ms but less than one second. Therefore adjust the ipc_timeout as 1
    second to avoid the heavy loading of system to cause ipc timeout.
    
    BUG=b:213980859
    TEST=emerge-cherry sys-kernel/chromeos-kernel-5_10
    
    Signed-off-by: YC Hung <yc.hung@mediatek.corp-partner.google.com>
    Change-Id: I2d897911f27b8c093442704130315ada881c5752
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3486640
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>

--- sound/soc/sof/sof-of-dev.c
+++ sound/soc/sof/sof-of-dev.c
@@ -58,6 +58,7 @@ static const struct sof_dev_desc sof_of_mt8195_desc = {
 	.default_fw_filename = "sof-mt8195.ri",
 	.nocodec_tplg_filename = "sof-mt8195-nocodec.tplg",
 	.ops = &sof_mt8195_ops,
+	.ipc_timeout = 1000,
 };
 #endif
 
