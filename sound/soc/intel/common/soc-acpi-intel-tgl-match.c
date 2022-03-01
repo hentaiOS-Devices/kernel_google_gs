ef358f453225d58b88efae1687baf28a1e4c91d1
balamurugan.c <balamurugan.c@intel.com>
2022-03-01 13:48:58 -0600

    BACKPORT: ASoC: Intel: add RT1308 I2S machine driver and HDMI-in capture via I2S support.
    Adding separate I2S machine driver for RT1308.
    
    Adding support for HDMI-In capture via I2S in slave mode configuration.
    
    Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Signed-off-by: balamurugan.c <balamurugan.c@intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20220301194903.60859-4-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    
    (cherry picked from commit da793fb0f56c0a53d0d461d80d9c1936a39afc30)
    
    Conflicts:
       sound/soc/intel/common/soc-acpi-intel-tgl-match.c
    
    BUG=b:186972464, b:220262207
    TEST=Submitter tests speaker output on Vell
    
    Signed-off-by: Brent Lu <brent.lu@intel.com>
    Change-Id: I71619cfc96602d9b785ec839d74917dcac2b2afa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3487188
    Tested-by: Terry Cheong <htcheong@chromium.org>
    Commit-Queue: Terry Cheong <htcheong@chromium.org>
    Reviewed-by: Terry Cheong <htcheong@chromium.org>

--- sound/soc/intel/common/soc-acpi-intel-tgl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-tgl-match.c
@@ -325,6 +325,11 @@ static const struct snd_soc_acpi_codecs tgl_rt5682_rt5682s_hp = {
 	.codecs = {"10EC5682", "RTL5682"},
 };
 
+static const struct snd_soc_acpi_codecs tgl_lt6911_hdmi = {
+	.num_codecs = 1,
+	.codecs = {"INTC10B0"}
+};
+
 struct snd_soc_acpi_mach snd_soc_acpi_intel_tgl_machines[] = {
 	{
 		.comp_ids = &tgl_rt5682_rt5682s_hp,
@@ -346,6 +351,13 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_tgl_machines[] = {
 		.machine_quirk = snd_soc_acpi_codec_list,
 		.quirk_data = &tgl_rt1011_amp,
 		.sof_tplg_filename = "sof-tgl-rt1011-rt5682.tplg",
+	},
+	{
+		.id = "10EC1308",
+		.drv_name = "tgl_rt1308_hdmi_ssp",
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &tgl_lt6911_hdmi,
+		.sof_tplg_filename = "sof-tgl-rt1308-ssp2-hdmi-ssp15.tplg"
 	},
 	{},
 };
