2a83e782ac694d8b4834654d41975a45a2c4f269
M Chetan Kumar <m.chetan.kumar@linux.intel.com>
2022-03-03 14:58:04 +0530

    CHROMIUM: net: wwan: t7xx: Devlink support for flashing & coredump collection
    1. Merged Devlink changes.
    2. Chrome integration bug fixes.
    
    TOREVERT=b:197895466
    BUG=b:221149675
    TEST=Manual connectivity/traffic tests on Brya and build with W=1
    
    Signed-off-by: M Chetan Kumar <m.chetan.kumar@linux.intel.com>
    Signed-off-by: Suchita Tiwari <suchita.tiwari@intel.corp-partner.google.com>
    Signed-off-by: Dinesh Sharma <dinesh.sharma@intel.corp-partner.google.com>
    Change-Id: I0ba528dcea94c3e81f98bb245279f769605e5379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3503760
    Tested-by: Dinesh Sharma <dinesh.sharma@intel.com>
    Reviewed-by: Daniel Winkler <danielwinkler@google.com>
    Tested-by: Daniel Winkler <danielwinkler@google.com>
    Commit-Queue: Daniel Winkler <danielwinkler@google.com>

--- drivers/net/wwan/t7xx/t7xx_pci.c
+++ drivers/net/wwan/t7xx/t7xx_pci.c
@@ -42,6 +42,7 @@
 #include "t7xx_reg.h"
 #include "t7xx_state_monitor.h"
 #include "t7xx_pci_rescan.h"
+#include "t7xx_port_devlink.h"
 
 #define T7XX_PCI_IREG_BASE		0
 #define T7XX_PCI_EREG_BASE		2
@@ -791,6 +792,8 @@ static int t7xx_pci_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	if (!t7xx_dev->hp_enable)
 		pci_ignore_hotplug(pdev);
 
+	t7xx_devlink_flash_debugfs_create();
+
 	return 0;
 }
 
@@ -811,6 +814,7 @@ static void t7xx_pci_remove(struct pci_dev *pdev)
 
 	kobject_put(pcie_drv_info_kobj);
 	pci_free_irq_vectors(t7xx_dev->pdev);
+	t7xx_devlink_flash_debugfs_remove();
 }
 
 static const struct pci_device_id t7xx_pci_table[] = {
