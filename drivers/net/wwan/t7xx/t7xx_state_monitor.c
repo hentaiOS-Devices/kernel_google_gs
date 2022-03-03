6e457046af8b79443ae4430532d384fa72af624d
Suchita Tiwari <suchita.tiwari@intel.corp-partner.google.com>
2022-03-03 11:10:05 +0530

    Revert "FROMLIST: net: wwan: t7xx: Add port proxy infrastructure"
    This reverts commit ae42be0533f93ed65e41f80966ea5b3295c94ca4.
    
    BUG=b:197895466
    
    Signed-off-by: Suchita Tiwari <suchita.tiwari@intel.corp-partner.google.com>
    Change-Id: Idc4b281c13f5cb92cf008e36abe7d3ed9750f0de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3503737
    Tested-by: Daniel Winkler <danielwinkler@google.com>
    Reviewed-by: Daniel Winkler <danielwinkler@google.com>
    Commit-Queue: Daniel Winkler <danielwinkler@google.com>

--- drivers/net/wwan/t7xx/t7xx_state_monitor.c
+++ drivers/net/wwan/t7xx/t7xx_state_monitor.c
@@ -38,7 +38,6 @@
 #include "t7xx_modem_ops.h"
 #include "t7xx_pci.h"
 #include "t7xx_pcie_mac.h"
-#include "t7xx_port_proxy.h"
 #include "t7xx_reg.h"
 #include "t7xx_state_monitor.h"
 
@@ -97,9 +96,6 @@ void t7xx_fsm_broadcast_state(struct t7xx_fsm_ctl *ctl, enum md_state state)
 		return;
 
 	ctl->md_state = state;
-
-	/* Update to port first, otherwise sending message on HS2 may fail */
-	t7xx_port_proxy_md_status_notify(ctl->md->port_prox, state);
 
 	fsm_state_notify(ctl->md, state);
 }
