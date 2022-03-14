3aff2834c1d601ebfd12b5206eea7221ede3e26c
Zong-Zhe Yang <kevin_yang@realtek.com>
2022-03-14 15:12:49 +0800

    FROMGIT: rtw89: reconstruct fw feature
    As the fw features gradually increase, it would be better that
    we have a set of methods to maintain fw features instead of using
    scattered bool variables.
    
    We reconstruct the way fw recognize features, and introduce
    RTW89_CHK_FW_FEATURE() / RTW89_SET_FW_FEATURE() to check / set
    fw features for uses.
    
    Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20220314071250.40292-8-pkshih@realtek.com
    (cherry picked from commit 11fe4ccda8672db5375f8bfcb209756a31c89a82
     git://git.kernel.org/pub/scm/linux/kernel/git/wireless/wireless-next.git main)
    
    BUG=b:197804708
    TEST=suite:wifi_matfunc suite:wifi_perf
    
    Change-Id: I5596b2081f167ddef063bc23bcc468545e15b93b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3560109
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>
    Tested-by: Matthew Wang <matthewmwang@chromium.org>
    Commit-Queue: Matthew Wang <matthewmwang@chromium.org>

--- drivers/net/wireless/realtek/rtw89/mac80211.c
+++ drivers/net/wireless/realtek/rtw89/mac80211.c
@@ -658,7 +658,7 @@ static int rtw89_ops_hw_scan(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 	struct rtw89_dev *rtwdev = hw->priv;
 	int ret = 0;
 
-	if (!rtwdev->fw.scan_offload)
+	if (!RTW89_CHK_FW_FEATURE(SCAN_OFFLOAD, &rtwdev->fw))
 		return 1;
 
 	if (rtwdev->scanning)
@@ -681,7 +681,7 @@ static void rtw89_ops_cancel_hw_scan(struct ieee80211_hw *hw,
 {
 	struct rtw89_dev *rtwdev = hw->priv;
 
-	if (!rtwdev->fw.scan_offload)
+	if (!RTW89_CHK_FW_FEATURE(SCAN_OFFLOAD, &rtwdev->fw))
 		return;
 
 	if (!rtwdev->scanning)
