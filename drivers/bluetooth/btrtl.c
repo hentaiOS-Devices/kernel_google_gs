43b7ad5a40b28e7e81af05a9815a915a6f3d5724
Archie Pusaka <apusaka@chromium.org>
2022-04-16 12:36:22 +0800

    CHROMIUM: Bluetooth: RTK prevent reset on suspend
    To shorten the suspend time on Guybrush/Dewatt. This would save about
    200ish ms.
    
    BUG=b:213014646, b:229065037
    TEST=Verify Dewatt doesn't reset on suspend when DUT isn't paired with
         any devices
    
    Signed-off-by: Archie Pusaka <apusaka@chromium.org>
    
    Change-Id: I5f47a7a0fcaf0b4333c664ff5f9a239262ba0b12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3589312
    Auto-Submit: Archie Pusaka <apusaka@chromium.org>
    Reviewed-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Tested-by: Archie Pusaka <apusaka@chromium.org>
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>

--- drivers/bluetooth/btrtl.c
+++ drivers/bluetooth/btrtl.c
@@ -742,11 +742,6 @@ void btrtl_set_quirks(struct hci_dev *hdev, struct btrtl_device_info *btrtl_dev)
 	/* Enable WBS supported for the specific Realtek devices. */
 	switch (btrtl_dev->project_id) {
 	case CHIP_ID_8822C:
-		/* Disallow RTL8822 to remote wakeup, in order to enter
-		 * global suspend and save power.
-		 */
-		set_bit(HCI_QUIRK_DISABLE_REMOTE_WAKE, &hdev->quirks);
-		fallthrough;
 	case CHIP_ID_8852A:
 	case CHIP_ID_8852C:
 		set_bit(HCI_QUIRK_VALID_LE_STATES, &hdev->quirks);
@@ -758,6 +753,18 @@ void btrtl_set_quirks(struct hci_dev *hdev, struct btrtl_device_info *btrtl_dev)
 		rtl_dev_dbg(hdev, "WBS supported not enabled.");
 		break;
 	}
+
+	/* Disallow RTL8822 to remote wakeup, in order to enter
+	 * global suspend and save power.
+	 */
+	if (CHIP_ID_8822C)
+		set_bit(HCI_QUIRK_DISABLE_REMOTE_WAKE, &hdev->quirks);
+
+	/* Force RTL8852A to enable remote wakeup in order to prevent it from
+	 * resetting itself and taking longer to resume from suspend
+	 */
+	if (CHIP_ID_8852A)
+		set_bit(HCI_QUIRK_FORCE_REMOTE_WAKE, &hdev->quirks);
 }
 EXPORT_SYMBOL_GPL(btrtl_set_quirks);
 
