1c2d14b369272f56e2e770a060b682951bf28863
Vamshi Krishna <vamshi.krishna.gopal@intel.com>
2022-05-09 12:09:20 -0500

    UPSTREAM: ASoC: Intel: sof_rt5682: Add support for adl_rt1019_rt5682s
    This patch adds the driver data for two rt1019 speaker amplifiers on
    SSP1 and rt5682s on SSP0 for ADL platform
    
    Reviewed-by: Brent Lu <brent.lu@intel.com>
    Signed-off-by: Vamshi Krishna <vamshi.krishna.gopal@intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20220509170922.54868-2-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 0b56ed197d7df394fc4918bfea0f77e6192996b0
     https://git.kernel.org/pub/scm/linux/kernel/git/broonie/sound.git for-next)
    
    BUG=b:261514697
    TEST=verify playback/capture functions
    
    Change-Id: I404df15e71dc18b65c819ab39086599ccda15970
    Signed-off-by: Mac Chiang <mac.chiang@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4084251
    Reviewed-by: Sean Paul <sean@poorly.run>
    Tested-by: Michael5 Chen <michael5_chen1@pegatron.corp-partner.google.com>
    Reviewed-by: Michael5 Chen <michael5_chen1@pegatron.corp-partner.google.com>
    Reviewed-by: Terry Cheong <htcheong@chromium.org>

--- sound/soc/intel/common/soc-acpi-intel-adl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-adl-match.c
@@ -363,6 +363,13 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_machines[] = {
 		.sof_tplg_filename = "sof-adl-max98360a-nau8825.tplg",
 	},
 	{
+		.id = "RTL5682",
+		.drv_name = "adl_rt1019_rt5682s",
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &adl_rt1019p_amp,
+		.sof_tplg_filename = "sof-adl-rt1019-rt5682.tplg",
+	},
+	{
 		.id = "10508825",
 		.drv_name = "adl_rt1015p_nau8825",
 		.machine_quirk = snd_soc_acpi_codec_list,
