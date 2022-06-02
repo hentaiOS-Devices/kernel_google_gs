932b200e28dd327311e57a2b0fbb39e9dd8e248b
Zong-Zhe Yang <kevin_yang@realtek.com>
2022-06-02 16:01:21 +0800

    Revert "UPSTREAM: rtw89: add tx_wake notify for low ps mode"
    This reverts commit 0b3c2ea00db95786eca822851a110fb510d7e019.
    
    BUG=b:232462809
    TEST=none
    
    To avoid backporting in the new patchset, it's reverted first.
    And then, a formal one that aligns with upstream's commit order
    will be re-landed in the following.
    
    Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    Change-Id: I8ec89dfa06528d3fb736eee54d6da03698b792d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3686654
    Commit-Queue: Arowa Suliman <arowa@chromium.org>
    Reviewed-by: Arowa Suliman <arowa@chromium.org>
    Tested-by: Arowa Suliman <arowa@chromium.org>

--- drivers/net/wireless/realtek/rtw89/core.h
+++ drivers/net/wireless/realtek/rtw89/core.h
@@ -2326,7 +2326,6 @@ struct rtw89_fw_info {
 	bool fw_log_enable;
 	bool old_ht_ra_format;
 	bool scan_offload;
-	bool tx_wake;
 };
 
 struct rtw89_cam_info {
@@ -2822,8 +2821,6 @@ struct rtw89_dev {
 	/* txqs to setup ba session */
 	struct list_head ba_list;
 	struct work_struct ba_work;
-	/* used to protect rpwm */
-	spinlock_t rpwm_lock;
 
 	struct rtw89_cam_info cam_info;
 
