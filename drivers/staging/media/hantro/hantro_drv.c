67c25ac518784bce0435083b9a40c2472ffda784
Hirokazu Honda <hiroh@chromium.org>
2022-06-23 12:25:49 +0900

    CHROMIUM: media: hantro: venc: Report supported bitrate modes
    The hantro driver supports constant bitrate mode only.
    The supported rate control mode is reported through querymenu() and
    s_ctrl() fails if non constant bitrate mode (e.g. VBR) is requested.
    
    BUG=b:182240945
    TEST=vidoe_encode_accelerator_tests
    
    Change-Id: I71f4983490b03fe9179b9faa73ba56a9c5f8c1c4
    Signed-off-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3719479
    Reviewed-by: Chen-Yu Tsai <wenst@chromium.org>

--- drivers/staging/media/hantro/hantro_drv.c
+++ drivers/staging/media/hantro/hantro_drv.c
@@ -601,6 +601,16 @@ static const struct hantro_ctrl controls[] = {
 			.def = V4L2_MPEG_VIDEO_H264_PROFILE_MAIN,
 		}
 	}, {
+		.codec = HANTRO_VP8_ENCODER | HANTRO_H264_ENCODER,
+		.cfg = {
+			.id = V4L2_CID_MPEG_VIDEO_BITRATE_MODE,
+			.min = V4L2_MPEG_VIDEO_BITRATE_MODE_CBR,
+			.max = V4L2_MPEG_VIDEO_BITRATE_MODE_CBR,
+			.menu_skip_mask = 0,
+			.step = 1,
+			.def = V4L2_MPEG_VIDEO_BITRATE_MODE_CBR,
+		}
+	}, {
 	},
 };
 
