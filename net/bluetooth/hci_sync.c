60fc6ede9bab4de025ec0418fd23983f0566c06a
Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
2022-07-25 13:50:53 -0700

    FROMLIST: Bluetooth: Always set event mask on suspend
    When suspending, always set the event mask once disconnects are
    successful. Otherwise, if wakeup is disallowed, the event mask is not
    set before suspend continues and can result in an early wakeup.
    
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    (am from https://patchwork.kernel.org/patch/12928408/)
    (also found at https://lore.kernel.org/r/20220725135026.1.Ia18502557c4ba9ba7cd2d1da2bae3aeb71b37e4e@changeid)
    
    BUG=b:238259387
    TEST=Manual test on Volteer on R105 with Beats headset
    
    Change-Id: Ia18502557c4ba9ba7cd2d1da2bae3aeb71b37e4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3781788
    Reviewed-by: Sean Paul <sean@poorly.run>
    Commit-Queue: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Tested-by: Zhengping Jiang <jiangzp@google.com>
    Reviewed-by: Zhengping Jiang <jiangzp@google.com>

--- net/bluetooth/hci_sync.c
+++ net/bluetooth/hci_sync.c
@@ -4977,6 +4977,9 @@ int hci_suspend_sync(struct hci_dev *hdev)
 		return err;
 	}
 
+	/* Update event mask so only the allowed event can wakeup the host */
+	hci_set_event_mask_sync(hdev);
+
 	/* Only configure accept list if disconnect succeeded and wake
 	 * isn't being prevented.
 	 */
@@ -4987,9 +4990,6 @@ int hci_suspend_sync(struct hci_dev *hdev)
 
 	/* Unpause to take care of updating scanning params */
 	hdev->scanning_paused = false;
-
-	/* Update event mask so only the allowed event can wakeup the host */
-	hci_set_event_mask_sync(hdev);
 
 	/* Enable event filter for paired devices */
 	hci_update_event_filter_sync(hdev);
