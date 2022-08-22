77a338a35c80e2eac2684b0248fdc08a5661aa1b
Guenter Roeck <groeck@chromium.org>
2022-08-22 07:33:31 -0700

    CHROMIUM: Revert "FROMLIST: futex/sched: add wake_up_process_prefer_current_cpu, use in FUTEX_SWAP"
    This reverts commit 2c89203e960aa2e4c481c602538ef739eba80265.
    
    BUG=b:170967073
    TEST=CQ
    
    Change-Id: I24f149d4d2d440247edb20f038cae1f0b27f47c8
    Disallow-Recycled-Builds: test-failures
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3845641
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6770,9 +6770,6 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	 */
 	lockdep_assert_held(&p->pi_lock);
 
-	if ((wake_flags & WF_CURRENT_CPU) && cpumask_test_cpu(cpu, p->cpus_ptr))
-		return cpu;
-
 	if (sd_flag & SD_BALANCE_WAKE) {
 		record_wakee(p);
 
