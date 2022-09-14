7f40dfa1213d45dcb3891add5730bb1d3f6f67d4
Judy Hsiao <judyhsiao@chromium.org>
2022-09-14 03:12:34 +0000

    FROMGIT: ASoC: rockchip: i2s: use regmap_read_poll_timeout to poll I2S_CLR
    Use regmap_read_poll_timeout to poll I2S_CLR.
    It also fixes the 'rockchip-i2s ff070000.i2s; fail to clear' when
    the read of I2S_CLR exceeds the retry limit.
    
    Fixes: 0ff9f8b9f592 ("ASoC: rockchip: i2s: Fix error code when fail to read I2S_CLR")
    Signed-off-by: Judy Hsiao <judyhsiao@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Link: https://lore.kernel.org/r/20220914031234.2250298-1-judyhsiao@chromium.org
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit fbb0ec656ee5ee43b4b3022fd8290707265c52df
     git://git.kernel.org/pub/scm/linux/kernel/git/broonie/sound.git for-next
    
    BUG=b:245061122, b:245058202, b:244664058
    TEST=`alsa_conformance_test -C hw:0,1 -r 11025`
    Signed-off-by: Judy Hsiao <judyhsiao@chromium.org>
    Change-Id: I37306e1d5ddefbc538b3700f93942048c6eb4803
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3893836
    Auto-Submit: Judy Hsiao <judyhsiao@google.com>
    Tested-by: Judy Hsiao <judyhsiao@google.com>
    Commit-Queue: Judy Hsiao <judyhsiao@google.com>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- sound/soc/rockchip/rockchip_i2s.c
+++ sound/soc/rockchip/rockchip_i2s.c
@@ -123,7 +123,6 @@ static inline struct rk_i2s_dev *to_info(struct snd_soc_dai *dai)
 static int rockchip_snd_txctrl(struct rk_i2s_dev *i2s, int on)
 {
 	unsigned int val = 0;
-	int retry = 10;
 	int ret = 0;
 
 	spin_lock(&i2s->lock);
@@ -160,18 +159,14 @@ static int rockchip_snd_txctrl(struct rk_i2s_dev *i2s, int on)
 						 I2S_CLR_TXC | I2S_CLR_RXC);
 			if (ret < 0)
 				goto end;
-			regmap_read(i2s->regmap, I2S_CLR, &val);
-
-			/* Should wait for clear operation to finish */
-			while (val) {
-				regmap_read(i2s->regmap, I2S_CLR, &val);
-				retry--;
-				if (!retry) {
-					dev_warn(i2s->dev, "fail to clear\n");
-					ret = -EBUSY;
-					break;
-				}
-			}
+			ret = regmap_read_poll_timeout(i2s->regmap,
+						       I2S_CLR,
+						       val,
+						       val != 0,
+						       20,
+						       200);
+			if (ret < 0)
+				dev_warn(i2s->dev, "fail to clear: %d\n", ret);
 		}
 	}
 
@@ -186,7 +181,6 @@ end:
 static int rockchip_snd_rxctrl(struct rk_i2s_dev *i2s, int on)
 {
 	unsigned int val = 0;
-	int retry = 10;
 	int ret = 0;
 	spin_lock(&i2s->lock);
 	if (on) {
@@ -223,18 +217,14 @@ static int rockchip_snd_rxctrl(struct rk_i2s_dev *i2s, int on)
 						 I2S_CLR_TXC | I2S_CLR_RXC);
 			if (ret < 0)
 				goto end;
-			regmap_read(i2s->regmap, I2S_CLR, &val);
-
-			/* Should wait for clear operation to finish */
-			while (val) {
-				regmap_read(i2s->regmap, I2S_CLR, &val);
-				retry--;
-				if (!retry) {
-					dev_warn(i2s->dev, "fail to clear\n");
-					ret = -EBUSY;
-					break;
-				}
-			}
+			ret = regmap_read_poll_timeout(i2s->regmap,
+						       I2S_CLR,
+						       val,
+						       val != 0,
+						       20,
+						       200);
+			if (ret < 0)
+				dev_warn(i2s->dev, "fail to clear: %d\n", ret);
 		}
 	}
 end:
