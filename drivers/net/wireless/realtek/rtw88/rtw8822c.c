e159e16ce50b17e568609c4e0a93e87238edf25d
Matthew Wang <matthewmwang@chromium.org>
2022-10-19 21:35:20 +0000

    Revert "BACKPORT: FROMGIT: wifi: rtw88: 8822c: extend supported probe request size"
    This reverts commit 1009ca89361dfe06da1d24140a2ecbf0f5194c07.
    
    Reason for revert: not applied correctly (need 8821c changes)
    
    Original change's description:
    > BACKPORT: FROMGIT: wifi: rtw88: 8822c: extend supported probe request size
    >
    > Some WSC IEs require size larger than we current supports. Extend size
    > to fit those demands. Separate the registered scan IE length by IC so
    > settings can be independent.
    >
    > Since old firmware uses fewer page number, define a firmware feature to
    > be compatible with various firmware version.
    >
    > Signed-off-by: Po-Hao Huang <phhuang@realtek.com>
    > Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    > Signed-off-by: Kalle Valo <kvalo@kernel.org>
    > Link: https://lore.kernel.org/r/20220727065003.28340-2-pkshih@realtek.com
    >
    > (cherry picked from commit d2eb7cb97c7df25df3e3e0f590b5bbf00c66d4c9
    >  git://git.kernel.org/pub/scm/linux/kernel/git/wireless/wireless-next.git main)
    >
    > Conflicts:
    >    drivers/net/wireless/realtek/rtw88/rtw8821c.c
    >    (8821c series is not merged)
    >    drivers/net/wireless/realtek/rtw88/rtw8822c.c
    >    (coex_para_ver and bt_desired_ver are not the same as upstream yet)
    >
    > BUG=b:231150841
    > TEST=test with zork; fix $BUG and potential problems;
    >      also, pass suite:wifi_matfunc
    >
    > Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    > Change-Id: Ie4ebe1f6ace03942830e3202a02863707e89d81c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3870561
    > Reviewed-by: Matthew Wang <matthewmwang@chromium.org>
    > Commit-Queue: Matthew Wang <matthewmwang@chromium.org>
    > Tested-by: Matthew Wang <matthewmwang@chromium.org>
    
    Bug: b:231150841
    Change-Id: I20ef4b8507a8b8d059e6e879c72272f6e10ff30a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3966681
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Tested-by: Matthew Wang <matthewmwang@chromium.org>
    Commit-Queue: Matthew Wang <matthewmwang@chromium.org>

--- drivers/net/wireless/realtek/rtw88/rtw8822c.c
+++ drivers/net/wireless/realtek/rtw88/rtw8822c.c
@@ -5301,7 +5301,7 @@ struct rtw_chip_info rtw8822c_hw_spec = {
 	.max_power_index = 0x7f,
 	.csi_buf_pg_num = 50,
 	.band = RTW_BAND_2G | RTW_BAND_5G,
-	.page_size = TX_PAGE_SIZE,
+	.page_size = 128,
 	.dig_min = 0x20,
 	.default_1ss_tx_path = BB_PATH_A,
 	.path_div_supported = true,
@@ -5346,7 +5346,6 @@ struct rtw_chip_info rtw8822c_hw_spec = {
 	.wowlan_stub = &rtw_wowlan_stub_8822c,
 	.max_sched_scan_ssids = 4,
 #endif
-	.max_scan_ie_len = (RTW_PROBE_PG_CNT - 1) * TX_PAGE_SIZE,
 	.coex_para_ver = 0x201029,
 	.bt_desired_ver = 0x1c,
 	.scbd_support = true,
