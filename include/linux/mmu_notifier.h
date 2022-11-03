935582328e8ee774719e729ca110eb035100ba91
Marek Maslanka <mm@semihalf.com>
2022-11-03 13:20:17 +0000

    Revert "CHROMIUM: mm: multigenerational lru: scan kvm mmu pages"
    This reverts commit 096a432b5e0916c1bb47e322d9a3316416a6154c.
    
    BUG=b:213475212
    TEST=
    
    Signed-off-by: Marek Maslanka <mm@semihalf.com>
    Change-Id: I35b8ef9d679d1c91d77222948bd07fd19ffadbc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4057610
    Reviewed-by: Yu Zhao <yuzhao@chromium.org>

--- include/linux/mmu_notifier.h
+++ include/linux/mmu_notifier.h
@@ -53,14 +53,6 @@ enum mmu_notifier_event {
 	MMU_NOTIFY_MIGRATE,
 };
 
-struct mmu_notifier_walk {
-	bool (*start_batch)(struct mm_struct *mm, void *priv);
-	bool (*end_batch)(void *priv, bool last);
-	struct page *(*get_page)(void *priv, unsigned long pfn, bool young);
-	void (*update_page)(void *priv, struct page *page);
-	void *private;
-};
-
 #define MMU_NOTIFIER_RANGE_BLOCKABLE (1 << 0)
 
 struct mmu_notifier_ops {
@@ -113,9 +105,6 @@ struct mmu_notifier_ops {
 			   struct mm_struct *mm,
 			   unsigned long start,
 			   unsigned long end);
-
-	void (*clear_young_walk)(struct mmu_notifier *mn,
-				 struct mmu_notifier_walk *walk);
 
 	/*
 	 * test_young is called to check the young/accessed bitflag in
@@ -396,8 +385,6 @@ extern int __mmu_notifier_clear_flush_young(struct mm_struct *mm,
 extern int __mmu_notifier_clear_young(struct mm_struct *mm,
 				      unsigned long start,
 				      unsigned long end);
-extern void __mmu_notifier_clear_young_walk(struct mm_struct *mm,
-					    struct mmu_notifier_walk *walk);
 extern int __mmu_notifier_test_young(struct mm_struct *mm,
 				     unsigned long address);
 extern void __mmu_notifier_change_pte(struct mm_struct *mm,
@@ -438,13 +425,6 @@ static inline int mmu_notifier_clear_young(struct mm_struct *mm,
 	if (mm_has_notifiers(mm))
 		return __mmu_notifier_clear_young(mm, start, end);
 	return 0;
-}
-
-static inline void mmu_notifier_clear_young_walk(struct mm_struct *mm,
-						 struct mmu_notifier_walk *walk)
-{
-	if (mm_has_notifiers(mm))
-		__mmu_notifier_clear_young_walk(mm, walk);
 }
 
 static inline int mmu_notifier_test_young(struct mm_struct *mm,
@@ -699,11 +679,6 @@ static inline int mmu_notifier_clear_flush_young(struct mm_struct *mm,
 					  unsigned long end)
 {
 	return 0;
-}
-
-static inline void mmu_notifier_clear_young_walk(struct mm_struct *mm,
-						 struct mmu_notifier_walk *walk)
-{
 }
 
 static inline int mmu_notifier_test_young(struct mm_struct *mm,
