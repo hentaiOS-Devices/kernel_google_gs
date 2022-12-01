54cdeed876cd509782a536199fd495924ea00ee3
Hsin-Yi Wang <hsinyi@chromium.org>
2022-12-01 19:02:05 +0800

    Revert "BACKPORT: FROMLIST: ath10k: add retry mechanism for ath10k_start"
    This reverts commit 4c2bd6b71743921dbea84f9b752c74c31d022294.
    
    The original patch is based on the assumption that ath10k should recover
    is SDIO failed. The retry mechanism was rejected upstream because of
    this[1].
    
    If normal suspend resume and wifi auto test won't hit the issue, it's
    acceptable to revert this patch.
    
    [1] https://patchwork.kernel.org/project/linux-wireless/patch/20200120025609.6060-1-wgong@codeaurora.org/#23154787
    
    TEST=suspend_stress_test
    TEST=tast.wifi.Prefer5Ghz
    BUG=b:147873677
    
    Change-Id: If361b8d5243c65e8c3b77229907db714443d3513
    Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4069381
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Commit-Queue: Douglas Anderson <dianders@chromium.org>

--- drivers/net/wireless/ath/ath10k/core.c
+++ drivers/net/wireless/ath/ath10k/core.c
@@ -193,7 +193,6 @@ static const struct ath10k_hw_params ath10k_hw_params_list[] = {
 		.credit_size_workaround = false,
 		.bmi_large_size_download = true,
 		.supports_peer_stats_info = true,
-		.start_retry = ATH10K_START_RETRY,
 	},
 	{
 		.id = QCA6174_HW_2_1_VERSION,
