2334558ddfcc49430baece1143367805ca186f3b
Jason-JH.Lin <jason-jh.lin@mediatek.com>
2023-04-04 21:29:43 +0800

    CHROMIUM: drm/mediatek: Remove debug log for fixing coverity issue
    Fix dereference after null check.
    
    BUG=b:277149724
    TEST="emerge-cherry sys-kernel/chromeos-kernel-5_10" pass
    
    Change-Id: I327e4663215e264ac5a59b074bfde9c90852f6db
    Signed-off-by: Jason-JH.Lin <jason-jh.lin@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4397387
    Reviewed-by: Fei Shao <fshao@chromium.org>
    Commit-Queue: Fei Shao <fshao@chromium.org>
    Tested-by: Fei Shao <fshao@chromium.org>

--- drivers/gpu/drm/mediatek/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mtk_drm_crtc.c
@@ -92,9 +92,6 @@ static void mtk_drm_crtc_finish_page_flip(struct mtk_drm_crtc *mtk_crtc)
 {
 	struct drm_crtc *crtc = &mtk_crtc->base;
 
-	if (!crtc->dev)
-		DRM_WARN("crtc 0x%px already free=== %s %d\n", crtc, __func__, __LINE__);
-
 	if (!mtk_crtc->event) {
 		DRM_WARN("crtc event 0x%px already free=== %s %d\n", mtk_crtc->event, __func__, __LINE__);
 		return;
@@ -109,9 +106,6 @@ static void mtk_drm_finish_page_flip(struct mtk_drm_crtc *mtk_crtc)
 {
 	struct drm_crtc *crtc = &mtk_crtc->base;
 	unsigned long flags;
-
-	if (!crtc->dev)
-		DRM_WARN("crtc 0x%px already free!!!! %s %d\n", crtc, __func__, __LINE__);
 
 	drm_crtc_handle_vblank(&mtk_crtc->base);
 
@@ -621,19 +615,8 @@ static void mtk_drm_crtc_update_config(struct mtk_drm_crtc *mtk_crtc,
 static void mtk_crtc_ddp_irq(void *data)
 {
 	struct drm_crtc *crtc = data;
-	struct mtk_drm_crtc *mtk_crtc;
-	struct mtk_drm_private *priv;
-
-	if (!crtc)
-		DRM_ERROR("%s crtc is null\n", __func__);
-	mtk_crtc = to_mtk_crtc(crtc);
-
-	if (!crtc->dev)
-		DRM_ERROR("%s crtc->dev is null\n", __func__);
-
-	if (!crtc->dev->dev_private)
-		DRM_ERROR("%s dev_private is null\n", __func__);
-	priv = crtc->dev->dev_private;
+	struct mtk_drm_crtc *mtk_crtc = to_mtk_crtc(crtc);
+	struct mtk_drm_private *priv = crtc->dev->dev_private;
 
 #if IS_REACHABLE(CONFIG_MTK_CMDQ)
 	if (!priv->data->shadow_register && !mtk_crtc->cmdq_client.chan)
