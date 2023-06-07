000254c8e8be1cbc2e5b74016d92ec764c990a25
Darren Hsu <darrenhsu@google.com>
2023-06-07 18:40:10 +0800

    vh: fs: ensure uniquness of eventpoll wakelock naming
    Bug: 281723757
    Test: Ensure that eventpoll wakelock naming is expected
    Change-Id: I11cc7a979219038e96299f509497ad9d2ba6816d
    Signed-off-by: Darren Hsu <darrenhsu@google.com>

--- drivers/soc/google/vh/kernel/fs/eventpoll.c
+++ drivers/soc/google/vh/kernel/fs/eventpoll.c
@@ -16,16 +16,19 @@
  * This part of code is vendor hook functions, which modify or extend the
  * original functions.
  */
+static atomic_t atomic_epoll_id = ATOMIC_INIT(0);
+
 void vh_ep_create_wakeup_source_mod(void *data, char *name, int len)
 {
 	char buf[64];
 	char task_comm_buf[sizeof(current->comm)];
+	int epoll_id = atomic_inc_return(&atomic_epoll_id);
 
 	get_task_comm(task_comm_buf, current);
 	strlcpy(buf, name, sizeof(buf));
 
 	if (!strncmp(name, "eventpoll", sizeof("eventpoll")))
-		snprintf(name, len, "epoll:%s", task_comm_buf);
+		snprintf(name, len, "epollm%d:%s", epoll_id, task_comm_buf);
 	else
-		snprintf(name, len, "epollitem:%s.%s", task_comm_buf, buf);
+		snprintf(name, len, "epolld%d:%s.%s", epoll_id, task_comm_buf, buf);
 }
