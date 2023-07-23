f4db071d0cb7f56eea5c76911bb117f8a39d6edf
Dyneteve <dyneteve@hentaios.com>
2023-07-23 14:27:05 +0000

    Revert "mm/huge_memory.c: make get_huge_zero_page() return bool"
    This reverts commit 2e3c1758e5f6a24ae64b8a6d9126d483c3e49f7e.

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -87,18 +87,18 @@ bool transparent_hugepage_active(struct vm_area_struct *vma)
 	return false;
 }
 
-static bool get_huge_zero_page(void)
+static struct page *get_huge_zero_page(void)
 {
 	struct page *zero_page;
 retry:
 	if (likely(atomic_inc_not_zero(&huge_zero_refcount)))
-		return true;
+		return READ_ONCE(huge_zero_page);
 
 	zero_page = alloc_pages((GFP_TRANSHUGE | __GFP_ZERO) & ~__GFP_MOVABLE,
 			HPAGE_PMD_ORDER);
 	if (!zero_page) {
 		count_vm_event(THP_ZERO_PAGE_ALLOC_FAILED);
-		return false;
+		return NULL;
 	}
 	count_vm_event(THP_ZERO_PAGE_ALLOC);
 	preempt_disable();
@@ -112,7 +112,7 @@ retry:
 	/* We take additional reference here. It will be put back by shrinker */
 	atomic_set(&huge_zero_refcount, 2);
 	preempt_enable();
-	return true;
+	return READ_ONCE(huge_zero_page);
 }
 
 static void put_huge_zero_page(void)
