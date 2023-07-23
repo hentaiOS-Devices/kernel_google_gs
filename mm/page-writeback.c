1b428f382f29cbf9c05ac1392c5ce48e3bdb56d2
Dyneteve <dyneteve@hentaios.com>
2023-07-23 14:27:18 +0000

    Revert "mm: hide laptop_mode_wb_timer entirely behind the BDI API"
    This reverts commit 6d180a6f971d923cac3290d7a74b42748fe46ecd.

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -2016,6 +2016,7 @@ int dirty_writeback_centisecs_handler(struct ctl_table *table, int write,
 	return ret;
 }
 
+#ifdef CONFIG_BLOCK
 void laptop_mode_timer_fn(struct timer_list *t)
 {
 	struct backing_dev_info *backing_dev_info =
@@ -2050,6 +2051,7 @@ void laptop_sync_completion(void)
 
 	rcu_read_unlock();
 }
+#endif
 
 /*
  * If ratelimit_pages is too high then we can get into dirty-data overload
