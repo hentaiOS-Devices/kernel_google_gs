fbcb06c53b5eb45afa46d0b6853d450b3781d6d5
Dyneteve <dyneteve@hentaios.com>
2023-07-23 14:27:14 +0000

    Revert "mm/hugetlb: grab head page refcount once for group of subpages"
    This reverts commit caed0826a7b273d2c0607311b8b45b2b974d4946.

--- mm/gup.c
+++ mm/gup.c
@@ -115,8 +115,9 @@ static inline struct page *try_get_compound_head(struct page *page, int refs)
  * considered failure, and furthermore, a likely bug in the caller, so a warning
  * is also emitted.
  */
-__maybe_unused struct page *try_grab_compound_head(struct page *page,
-						   int refs, unsigned int flags)
+static __maybe_unused struct page *try_grab_compound_head(struct page *page,
+							  int refs,
+							  unsigned int flags)
 {
 	bool vendor_ret = false;
 
