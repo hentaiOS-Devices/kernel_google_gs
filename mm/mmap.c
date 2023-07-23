aaf87e06f9e6d6042d61ddbe2818f26d8f4deb63
Dyneteve <dyneteve@hentaios.com>
2023-07-23 14:27:13 +0000

    Revert "mm: ignore MAP_EXECUTABLE in ksys_mmap_pgoff()"
    This reverts commit ddba760c3a03a73d440d691c5b5cc730c640c5d2.

--- mm/mmap.c
+++ mm/mmap.c
@@ -1697,7 +1697,7 @@ unsigned long ksys_mmap_pgoff(unsigned long addr, unsigned long len,
 			return PTR_ERR(file);
 	}
 
-	flags &= ~MAP_DENYWRITE;
+	flags &= ~(MAP_EXECUTABLE | MAP_DENYWRITE);
 
 	retval = vm_mmap_pgoff(file, addr, len, prot, flags, pgoff);
 out_fput:
--- mm/nommu.c
+++ mm/nommu.c
@@ -1306,7 +1306,7 @@ unsigned long ksys_mmap_pgoff(unsigned long addr, unsigned long len,
 			goto out;
 	}
 
-	flags &= ~MAP_DENYWRITE;
+	flags &= ~(MAP_EXECUTABLE | MAP_DENYWRITE);
 
 	retval = vm_mmap_pgoff(file, addr, len, prot, flags, pgoff);
 
