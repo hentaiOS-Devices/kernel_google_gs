f31a603e689613192b89082b0988ba573429b7d6
Qais Yousef <qyousef@google.com>
2023-09-25 14:55:59 +0000

    vh: sched: Don't filter iowait boosted tasks
    iowait boosted tasks can run for a short period of time but they need to
    run faster to help get better IO performance. Hence don't filter them
    out.
    
    Bug: 301853335
    Change-Id: Ia5319805d5c2f0b6de963b9d33aa284e2ffa704b
    Signed-off-by: Qais Yousef <qyousef@google.com>

--- drivers/soc/google/vh/kernel/sched/sched_priv.h
+++ drivers/soc/google/vh/kernel/sched/sched_priv.h
@@ -525,6 +525,9 @@ static inline bool uclamp_can_ignore_uclamp_min(struct rq *rq,
 	if (get_uclamp_fork_reset(p, true))
 		return false;
 
+	if (p->in_iowait && uclamp_boosted(p))
+		return false;
+
 	if (rt_task(p))
 		return task_util(p) < sysctl_sched_uclamp_min_filter_rt;
 
@@ -598,6 +601,9 @@ static inline bool uclamp_can_ignore_uclamp_max(struct rq *rq,
 		return false;
 
 	if (get_uclamp_fork_reset(p, true))
+		return false;
+
+	if (p->in_iowait && uclamp_boosted(p))
 		return false;
 
 	/*
